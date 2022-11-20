b0a63886d94b484bfc27da313a787d16fc29c9a1
farzadafi <farzadafi40@gmail.com>
2022-11-20 16:41:02 +0330

    create Spring_Retry module

---
+++ Spring_Retry/src/main/java/com/example/spring_retry/SpringRetryApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_retry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRetryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRetryApplication.class, args);
+    }
+
+}
---
+++ Spring_Retry/src/test/java/com/example/spring_retry/SpringRetryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_retry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRetryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
