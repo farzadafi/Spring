b7ad11aebb157f71e1f550efcfd3d8d7dc02cb33
farzadafi <farzadafi40@gmail.com>
2022-11-21 13:48:39 +0330

    create JwtConfig

---
+++ Spring_Security/src/main/java/com/example/spring_security/jwt/JwtConfig.java
@@ -0,0 +1,28 @@
+package com.example.spring_security.jwt;
+
+import com.google.common.net.HttpHeaders;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@ConfigurationProperties(prefix = "application.jwt")
+@NoArgsConstructor
+@Getter
+@Setter
+@Configuration
+public class JwtConfig {
+
+    private String secretKey;
+    private String tokenPrefix;
+    private Integer tokenExpirationAfterDays;
+
+    public String getAuthorizationHeader() {
+        return HttpHeaders.AUTHORIZATION;
+    }
+
+    public void setTokenExpirationAfterDays(Integer tokenExpirationAfterDays) {
+        this.tokenExpirationAfterDays = tokenExpirationAfterDays;
+    }
+}
