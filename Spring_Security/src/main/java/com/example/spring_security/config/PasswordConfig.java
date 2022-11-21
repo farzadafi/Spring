0e24a5906234dae324838eba72d4cc1f4b50bcfb
farzadafi <farzadafi40@gmail.com>
2022-11-21 13:37:51 +0330

    create passwordConfig class

---
+++ Spring_Security/src/main/java/com/example/spring_security/config/PasswordConfig.java
@@ -0,0 +1,14 @@
+package com.example.spring_security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class PasswordConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
