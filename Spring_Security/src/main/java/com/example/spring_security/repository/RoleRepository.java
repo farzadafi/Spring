68eeed0cd14f06b10dc8c635fea0d315e5a19707
farzadafi <farzadafi40@gmail.com>
2022-11-21 13:52:30 +0330

    create RoleRepository

---
+++ Spring_Security/src/main/java/com/example/spring_security/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.example.spring_security.repository;
+
+import com.example.spring_security.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    Optional<Role> findByName(String name);
+}
