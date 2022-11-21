3fb2665a24d26f43663dc02f3c91e9e39077d358
farzadafi <farzadafi40@gmail.com>
2022-11-21 13:48:21 +0330

    create FailSaveException

---
+++ Spring_Security/src/main/java/com/example/spring_security/exception/FailSaveException.java
@@ -0,0 +1,8 @@
+package com.example.spring_security.exception;
+
+public class FailSaveException extends RuntimeException {
+
+    public FailSaveException(String message) {
+        super(message);
+    }
+}
