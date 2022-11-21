3500305eac76645d24a4def2b936723181f999a6
farzadafi <farzadafi40@gmail.com>
2022-11-21 13:54:10 +0330

    create UserRepository

---
+++ Spring_Security/src/main/java/com/example/spring_security/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.spring_security.repository;
+
+import com.example.spring_security.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+    Optional<User> findByUsername(String username);
+}
