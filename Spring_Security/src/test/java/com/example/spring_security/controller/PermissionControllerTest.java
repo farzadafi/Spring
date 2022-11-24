a9a18bb3eb8e97d8dd5319e45584cfea06f940d7
farzadafi <farzadafi40@gmail.com>
2022-11-24 09:07:11 +0330

    write body of addPermission method

--- Spring_Security/src/test/java/com/example/spring_security/controller/PermissionControllerTest.java
+++ Spring_Security/src/test/java/com/example/spring_security/controller/PermissionControllerTest.java
@@ -1,18 +1,50 @@
 package com.example.spring_security.controller;
 
+import com.example.spring_security.model.Permission;
+import com.example.spring_security.model.Role;
+import com.example.spring_security.model.User;
+import com.google.common.collect.Sets;
+import net.minidev.json.JSONObject;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.annotation.Order;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.jdbc.datasource.init.ScriptUtils;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class PermissionControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    Permission permission = new Permission("permission:write");
+    Role role = new Role("ROLE_ADMIN", Sets.newHashSet(permission), true);
+    User admin = new User("admin", "admin", Sets.newHashSet(role), true);
 
     @BeforeAll
     static void setup(@Autowired DataSource dataSource) {
@@ -24,6 +56,18 @@ class PermissionControllerTest {
     }
 
     @Test
-    void addPermission() {
+    @Order(1)
+    void addPermission() throws Exception {
+        JSONObject permissionJson = new JSONObject();
+        permissionJson.put("name", "test");
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/permission/addPermission")
+                .with(SecurityMockMvcRequestPostProcessors.user(admin))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(permissionJson.toJSONString());
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        MockHttpServletResponse response = result.getResponse();
+
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
     }
 }
