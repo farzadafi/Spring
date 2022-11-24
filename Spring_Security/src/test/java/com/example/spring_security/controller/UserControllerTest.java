0e5b77aada55bc8edff14024a315e64c7c22802c
farzadafi <farzadafi40@gmail.com>
2022-11-24 09:03:38 +0330

    create UserControllerTest

---
+++ Spring_Security/src/test/java/com/example/spring_security/controller/UserControllerTest.java
@@ -0,0 +1,20 @@
+package com.example.spring_security.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    @Test
+    void addUser() {
+    }
+
+    @Test
+    void userWriteTest() {
+    }
+
+    @Test
+    void userReadTest() {
+    }
+}
