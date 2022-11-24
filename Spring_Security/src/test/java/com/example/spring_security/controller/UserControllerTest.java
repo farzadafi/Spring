9fa0de09217fa9594f6b1d518b2f3c7fe8a9aa72
farzadafi <farzadafi40@gmail.com>
2022-11-24 09:13:18 +0330

    write body of addUser method

--- Spring_Security/src/test/java/com/example/spring_security/controller/UserControllerTest.java
+++ Spring_Security/src/test/java/com/example/spring_security/controller/UserControllerTest.java
@@ -1,16 +1,25 @@
 package com.example.spring_security.controller;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import com.example.spring_security.model.Permission;
+import com.example.spring_security.model.Role;
+import com.example.spring_security.model.User;
+import com.google.common.collect.Sets;
+import net.minidev.json.JSONObject;
+import org.junit.jupiter.api.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.jdbc.datasource.init.ScriptUtils;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -24,6 +33,14 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class UserControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    Permission writePermission = new Permission("user:write");
+    Permission readPermission = new Permission("user:read");
+    Role role = new Role("ROLE_ADMIN", Sets.newHashSet(writePermission, readPermission), true);
+    User admin = new User("admin", "admin", Sets.newHashSet(role), true);
+
     @BeforeAll
     static void setup(@Autowired DataSource dataSource) {
         try (Connection connection = dataSource.getConnection()) {
@@ -34,7 +51,19 @@ class UserControllerTest {
     }
 
     @Test
-    void addUser() {
+    @Order(1)
+    void addUser() throws Exception {
+        JSONObject userJson = new JSONObject();
+        userJson.put("username", "farzadafi");
+        userJson.put("password", "aA1!aaaa");
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/api/v1/user/addUser")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(userJson.toJSONString());
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        MockHttpServletResponse response = result.getResponse();
+
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
     }
 
     @Test
