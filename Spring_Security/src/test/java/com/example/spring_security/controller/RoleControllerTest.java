c9d00f1af286e741c3513b73501e201023f87a42
farzadafi <farzadafi40@gmail.com>
2022-11-24 09:03:30 +0330

    create RoleControllerTest

---
+++ Spring_Security/src/test/java/com/example/spring_security/controller/RoleControllerTest.java
@@ -0,0 +1,16 @@
+package com.example.spring_security.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RoleControllerTest {
+
+    @Test
+    void addRole() {
+    }
+
+    @Test
+    void assignPermissionToRole() {
+    }
+}
