8420651609c48c7677c978b261b13cfecd9e90ad
farzadafi <farzadafi40@gmail.com>
2022-11-24 09:00:17 +0330

    create permissionControllerData.sql for insert data to DB before run test

---
+++ Spring_Security/src/test/resources/permissionControllerData.sql
@@ -0,0 +1,5 @@
+insert into permission (name) values ('permission:write');
+insert into role (is_enabled, name) values (true, 'ROLE_ADMIN');
+insert into roles_permissions (role_id, permission_id) values (1, 1);
+insert into users (is_enabled, password, username)values (true, 'admin', 'admin');
+insert into users_roles (user_id, role_id) values (1, 1);
