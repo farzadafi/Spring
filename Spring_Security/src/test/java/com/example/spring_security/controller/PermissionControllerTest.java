1e4756c83826670ae7867937d5cf61c6e72f4559
farzadafi <farzadafi40@gmail.com>
2022-11-24 09:03:18 +0330

    create PermissionControllerTest

---
+++ Spring_Security/src/test/java/com/example/spring_security/controller/PermissionControllerTest.java
@@ -0,0 +1,12 @@
+package com.example.spring_security.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PermissionControllerTest {
+
+    @Test
+    void addPermission() {
+    }
+}
