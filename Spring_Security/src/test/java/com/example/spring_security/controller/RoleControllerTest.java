181c7b10c0ccdd254f8d00bc478a175b7200fce2
farzadafi <farzadafi40@gmail.com>
2022-11-24 09:08:09 +0330

    create setup method for run BeforeAll test and insert data to DB

--- Spring_Security/src/test/java/com/example/spring_security/controller/RoleControllerTest.java
+++ Spring_Security/src/test/java/com/example/spring_security/controller/RoleControllerTest.java
@@ -1,10 +1,37 @@
 package com.example.spring_security.controller;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class RoleControllerTest {
+
+    @BeforeAll
+    static void setup(@Autowired DataSource dataSource) {
+        try (Connection connection = dataSource.getConnection()) {
+            ScriptUtils.executeSqlScript(connection, new ClassPathResource("roleControllerData.sql"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Test
     void addRole() {
