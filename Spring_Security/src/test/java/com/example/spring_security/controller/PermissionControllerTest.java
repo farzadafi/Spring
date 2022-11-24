6e51053c3fe9cb18bddf68983c63572e04303fd9
farzadafi <farzadafi40@gmail.com>
2022-11-24 09:04:42 +0330

    add setup method for run beforeAll test

--- Spring_Security/src/test/java/com/example/spring_security/controller/PermissionControllerTest.java
+++ Spring_Security/src/test/java/com/example/spring_security/controller/PermissionControllerTest.java
@@ -1,10 +1,27 @@
 package com.example.spring_security.controller;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PermissionControllerTest {
+
+    @BeforeAll
+    static void setup(@Autowired DataSource dataSource) {
+        try (Connection connection = dataSource.getConnection()) {
+            ScriptUtils.executeSqlScript(connection, new ClassPathResource("permissionControllerData.sql"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Test
     void addPermission() {
