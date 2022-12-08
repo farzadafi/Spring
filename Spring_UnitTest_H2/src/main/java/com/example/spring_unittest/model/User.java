1f185aa09b0e369d702482fc372c516a8f720965
farzadafi <farzadafi40@gmail.com>
2022-12-08 17:52:43 +0330

    add some field and lombok annotation

--- Spring_UnitTest_H2/src/main/java/com/example/spring_unittest/model/User.java
+++ Spring_UnitTest_H2/src/main/java/com/example/spring_unittest/model/User.java
@@ -1,4 +1,22 @@
 package com.example.spring_unittest.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String username;
+
+    private String password;
 }
