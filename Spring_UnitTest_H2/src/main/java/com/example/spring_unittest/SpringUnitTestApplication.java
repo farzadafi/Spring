9025813500a8a8291f66fb4e2f56b84d048ef747
farzadafi <farzadafi40@gmail.com>
2022-12-08 17:43:01 +0330

    create spring_UnitTest_H2 module

---
+++ Spring_UnitTest_H2/src/main/java/com/example/spring_unittest/SpringUnitTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_unittest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUnitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUnitTestApplication.class, args);
+    }
+
+}
