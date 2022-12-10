176e8e32b2e235ec2d4fb4ccf7dfce7c8f74987e
farzadafi <farzadafi40@gmail.com>
2022-12-10 20:42:32 +0330

    add register method

--- Spring_UnitTest_H2/src/main/java/com/example/spring_unittest/controller/UserController.java
+++ Spring_UnitTest_H2/src/main/java/com/example/spring_unittest/controller/UserController.java
@@ -1,4 +1,24 @@
 package com.example.spring_unittest.controller;
 
+import com.example.spring_unittest.dto.UserDto;
+import com.example.spring_unittest.model.User;
+import com.example.spring_unittest.service.user.UserServiceImpel;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
+    private final UserServiceImpel userService;
+
+    public UserController(UserServiceImpel userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@RequestBody UserDto userDto) {
+        User user = new User(null, userDto.getUsername(), userDto.getPassword());
+        userService.register(user);
+        return "OK";
+    }
 }
