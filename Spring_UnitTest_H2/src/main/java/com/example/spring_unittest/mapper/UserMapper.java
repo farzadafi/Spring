1a597d6fb1289e307974dd39337a1c2837533538
farzadafi <farzadafi40@gmail.com>
2022-12-10 20:54:27 +0330

    add dtoToModel class

--- Spring_UnitTest_H2/src/main/java/com/example/spring_unittest/mapper/UserMapper.java
+++ Spring_UnitTest_H2/src/main/java/com/example/spring_unittest/mapper/UserMapper.java
@@ -1,4 +1,14 @@
 package com.example.spring_unittest.mapper;
 
+import com.example.spring_unittest.dto.UserDto;
+import com.example.spring_unittest.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
 public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    User dtoToModel(UserDto userDto);
 }
