4b3842c3c84626665bd22f73756f5f9cdadc21f5
farzadafi <farzadafi40@gmail.com>
2022-12-10 20:40:38 +0330

    add ResponseStatus

--- Spring_UnitTest_H2/src/main/java/com/example/spring_unittest/exception/SaveFailException.java
+++ Spring_UnitTest_H2/src/main/java/com/example/spring_unittest/exception/SaveFailException.java
@@ -1,7 +1,9 @@
 package com.example.spring_unittest.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(value = HttpStatus.NOT_ACCEPTABLE, reason = "please try again")
 public class SaveFailException extends RuntimeException{
 
     public SaveFailException(String message) {
