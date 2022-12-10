6a4a1dbc27557f28d71b2cc2bb5095037b294287
farzadafi <farzadafi40@gmail.com>
2022-12-10 20:42:21 +0330

    fix register method

--- Spring_UnitTest_H2/src/main/java/com/example/spring_unittest/service/user/UserService.java
+++ Spring_UnitTest_H2/src/main/java/com/example/spring_unittest/service/user/UserService.java
@@ -1,4 +1,7 @@
 package com.example.spring_unittest.service.user;
 
+import com.example.spring_unittest.model.User;
+
 public interface UserService {
+    void register(User user);
 }
--- Spring_UnitTest_H2/src/main/java/com/example/spring_unittest/service/user/UserServiceImpel.java
+++ Spring_UnitTest_H2/src/main/java/com/example/spring_unittest/service/user/UserServiceImpel.java
@@ -1,4 +1,22 @@
 package com.example.spring_unittest.service.user;
 
+import com.example.spring_unittest.exception.SaveFailException;
+import com.example.spring_unittest.model.User;
+import com.example.spring_unittest.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserServiceImpel implements UserService{
+
+    private final UserRepository repository;
+
+    public UserServiceImpel(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public void register(User user) {
+        if(repository.save(user).getId() == null)
+            throw new SaveFailException(String.format("fail to save %s", user.getUsername()));
+    }
 }
