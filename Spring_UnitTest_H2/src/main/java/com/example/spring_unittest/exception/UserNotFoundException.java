8aa5474e07d9fb16fb263d82b304c20a15e7bc4e
farzadafi <farzadafi40@gmail.com>
2022-12-10 21:02:40 +0330

    add @ResponseStatus

--- Spring_UnitTest_H2/src/main/java/com/example/spring_unittest/exception/UserNotFoundException.java
+++ Spring_UnitTest_H2/src/main/java/com/example/spring_unittest/exception/UserNotFoundException.java
@@ -1,5 +1,9 @@
 package com.example.spring_unittest.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "user not found")
 public class UserNotFoundException extends RuntimeException{
 
     public UserNotFoundException(String message) {
