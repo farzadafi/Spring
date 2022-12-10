33b45d31516c52d6d32ebb825df4104f104b9b3e
farzadafi <farzadafi40@gmail.com>
2022-12-10 20:47:19 +0330

    add findById method

--- Spring_UnitTest_H2/src/main/java/com/example/spring_unittest/service/user/UserService.java
+++ Spring_UnitTest_H2/src/main/java/com/example/spring_unittest/service/user/UserService.java
@@ -2,6 +2,9 @@ package com.example.spring_unittest.service.user;
 
 import com.example.spring_unittest.model.User;
 
+import java.util.Optional;
+
 public interface UserService {
     void register(User user);
+    Optional<User> findById(Integer id);
 }
--- Spring_UnitTest_H2/src/main/java/com/example/spring_unittest/service/user/UserServiceImpel.java
+++ Spring_UnitTest_H2/src/main/java/com/example/spring_unittest/service/user/UserServiceImpel.java
@@ -5,6 +5,8 @@ import com.example.spring_unittest.model.User;
 import com.example.spring_unittest.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class UserServiceImpel implements UserService{
 
@@ -18,5 +20,10 @@ public class UserServiceImpel implements UserService{
     public void register(User user) {
         if(repository.save(user).getId() == null)
             throw new SaveFailException(String.format("fail to save %s", user.getUsername()));
+    }
+
+    @Override
+    public Optional<User> findById(Integer id) {
+        return repository.findById(id);
     }
 }
