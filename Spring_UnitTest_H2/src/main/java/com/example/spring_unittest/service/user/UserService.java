7b7ac8b610ebe5cbb7400777b95a34f8e008ef56
farzadafi <farzadafi40@gmail.com>
2022-12-10 20:03:52 +0330

    create UserService and UserServiceImpel

---
+++ Spring_UnitTest_H2/src/main/java/com/example/spring_unittest/service/user/UserService.java
@@ -0,0 +1,4 @@
+package com.example.spring_unittest.service.user;
+
+public interface UserService {
+}
---
+++ Spring_UnitTest_H2/src/main/java/com/example/spring_unittest/service/user/UserServiceImpel.java
@@ -0,0 +1,4 @@
+package com.example.spring_unittest.service.user;
+
+public class UserServiceImpel implements UserService{
+}
