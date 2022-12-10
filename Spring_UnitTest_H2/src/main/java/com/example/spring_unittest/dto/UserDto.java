665a37b5008e56407906c083bb25780b1d11ed74
farzadafi <farzadafi40@gmail.com>
2022-12-10 20:08:21 +0330

    add some field like User class

--- Spring_UnitTest_H2/src/main/java/com/example/spring_unittest/dto/UserDto.java
+++ Spring_UnitTest_H2/src/main/java/com/example/spring_unittest/dto/UserDto.java
@@ -1,4 +1,16 @@
 package com.example.spring_unittest.dto;
 
+import lombok.*;
+
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
 public class UserDto {
+    private Integer id;
+
+    private String username;
+
+    private String password;
 }
