493a7c89ce0297a7aac50402ae06679de428da85
farzadafi <farzadafi40@gmail.com>
2022-12-11 20:01:01 +0330

    add findByUsername method

--- Spring_UnitTest_H2/src/main/java/com/example/spring_unittest/repository/UserRepository.java
+++ Spring_UnitTest_H2/src/main/java/com/example/spring_unittest/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.example.spring_unittest.repository;
 import com.example.spring_unittest.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
 }
