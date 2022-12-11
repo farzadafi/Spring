1d79b80a2659774181dbb1f07cb6a6b57c351b3e
farzadafi <farzadafi40@gmail.com>
2022-12-11 20:31:41 +0330

    add findByUsername_userExists_true method

--- Spring_UnitTest_H2/src/test/java/com/example/spring_unittest/repository/UserRepositoryTest.java
+++ Spring_UnitTest_H2/src/test/java/com/example/spring_unittest/repository/UserRepositoryTest.java
@@ -1,7 +1,35 @@
 package com.example.spring_unittest.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.example.spring_unittest.model.User;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.core.annotation.Order;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class UserRepositoryTest {
 
+    @Autowired
+    private UserRepository repository;
+
+    @Test
+    @Order(1)
+    void findByUsername_userExists_true() {
+        //given
+        User user = User.builder().username("farzadafi").password("abc").build();
+        repository.save(user);
+
+        //when
+        Optional<User> optionalUser = repository.findByUsername(user.getUsername());
+
+        //then
+        assertThat(optionalUser.isPresent()).isTrue();
+    }
 }
