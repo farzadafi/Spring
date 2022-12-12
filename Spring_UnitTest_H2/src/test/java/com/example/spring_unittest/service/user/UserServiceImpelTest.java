daad296fe1d3025b80d259d290401b9d9b710f79
farzadafi <farzadafi40@gmail.com>
2022-12-12 14:32:45 +0330

    rename findAll to canGetAllUser and implement body

--- Spring_UnitTest_H2/src/test/java/com/example/spring_unittest/service/user/UserServiceImpelTest.java
+++ Spring_UnitTest_H2/src/test/java/com/example/spring_unittest/service/user/UserServiceImpelTest.java
@@ -1,14 +1,33 @@
 package com.example.spring_unittest.service.user;
 
+import com.example.spring_unittest.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 class UserServiceImpelTest {
 
+    @Mock
+    private UserRepository userRepository;
+    private UserServiceImpel userService;
+
+    @BeforeEach
+    void setUp() {
+        userService = new UserServiceImpel(userRepository);
+    }
+
     @Test
-    void findAll() {
+    void canGetAllUser() {
+        //when
+        userService.findAll();
+        //then
+        verify(userRepository).findAll();
     }
 
     @Test
