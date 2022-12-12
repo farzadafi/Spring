a5d7fb7a8dab27c7b8b290652a2222996d895632
farzadafi <farzadafi40@gmail.com>
2022-12-12 14:27:23 +0330

    create UserServiceImpelTest class

---
+++ Spring_UnitTest_H2/src/test/java/com/example/spring_unittest/service/user/UserServiceImpelTest.java
@@ -0,0 +1,28 @@
+package com.example.spring_unittest.service.user;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceImpelTest {
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    @Disabled
+    void register() {
+    }
+
+    @Test
+    @Disabled
+    void findById() {
+    }
+
+    @Test
+    @Disabled
+    void findByUsername() {
+    }
+}
