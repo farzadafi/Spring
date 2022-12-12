2d84dbd82e3d228fe556fb24aec840b2c8a65048
farzadafi <farzadafi40@gmail.com>
2022-12-12 15:53:18 +0330

    implements body canRegisterUser method

--- Spring_UnitTest_H2/src/test/java/com/example/spring_unittest/service/user/UserServiceImpelTest.java
+++ Spring_UnitTest_H2/src/test/java/com/example/spring_unittest/service/user/UserServiceImpelTest.java
@@ -6,9 +6,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -32,8 +34,19 @@ class UserServiceImpelTest {
     }
 
     @Test
-    @Disabled
-    void register() {
+    void canRegisterUser() {
+        //given
+        User user = User.builder().username("farzadafi").password("abc").build();
+
+        //when
+        userService.register(user);
+
+        //then
+        ArgumentCaptor<User> userArgumentCaptor =
+                ArgumentCaptor.forClass(User.class);
+        verify(userRepository).save(userArgumentCaptor.capture());
+        User userAfterSave = userArgumentCaptor.getValue();
+        assertThat(userAfterSave).isEqualTo(user);
     }
 
     @Test
