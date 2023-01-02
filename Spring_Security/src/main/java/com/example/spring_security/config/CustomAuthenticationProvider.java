6c1bd7aacc032d7b8693f9e9bc8a68b71cc793a3
farzadafi <farzadafi40@gmail.com>
2023-01-02 07:39:32 +0330

    implements AuthenticationProvider interface

--- Spring_Security/src/main/java/com/example/spring_security/config/CustomAuthenticationProvider.java
+++ Spring_Security/src/main/java/com/example/spring_security/config/CustomAuthenticationProvider.java
@@ -1,4 +1,18 @@
 package com.example.spring_security.config;
 
-public class CustomAuthenticationProvider {
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return false;
+    }
 }
