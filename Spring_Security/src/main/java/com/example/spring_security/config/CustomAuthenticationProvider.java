e925cd1330350abd0e4e7ad13192f1b61d4b0122
farzadafi <farzadafi40@gmail.com>
2023-01-02 07:41:25 +0330

    implements authenticate body

--- Spring_Security/src/main/java/com/example/spring_security/config/CustomAuthenticationProvider.java
+++ Spring_Security/src/main/java/com/example/spring_security/config/CustomAuthenticationProvider.java
@@ -1,14 +1,26 @@
 package com.example.spring_security.config;
 
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.thymeleaf.util.StringUtils;
+
+import java.util.ArrayList;
 
 public class CustomAuthenticationProvider implements AuthenticationProvider {
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        return null;
+        String name = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        if (!StringUtils.isEmpty(name)) {
+            return new UsernamePasswordAuthenticationToken(
+                    name, password, new ArrayList<>());
+        } else {
+            return null;
+        }
     }
 
     @Override
