dffc905fa83de1995b142a04e6762520d804f19e
farzadafi <farzadafi40@gmail.com>
2023-01-05 05:43:57 +0330

    add test method

--- server_test/src/main/java/com/example/server_test/controller/TestController.java
+++ server_test/src/main/java/com/example/server_test/controller/TestController.java
@@ -1,4 +1,13 @@
 package com.example.server_test.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/test")
 public class TestController {
+
+    @GetMapping("/get-test")
+    public String test() {
+        "this is a test String from 8085 server"
+    }
 }
