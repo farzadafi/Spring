5775833d404b3b8795be664a388ea0ed88bc249a
farzadafi <farzadafi40@gmail.com>
2023-01-05 05:09:26 +0330

    create Spring_Gateway module

---
+++ Spring_Gateway/src/main/java/com/example/spring_gateway/SpringGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGatewayApplication.class, args);
+    }
+
+}
---
+++ Spring_Gateway/src/test/java/com/example/spring_gateway/SpringGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
