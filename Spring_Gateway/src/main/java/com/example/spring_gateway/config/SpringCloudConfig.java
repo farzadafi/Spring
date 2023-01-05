4a0d8870a0fb24b4e0f70d83033a09426cff4fed
farzadafi <farzadafi40@gmail.com>
2023-01-05 06:02:35 +0330

    write a @Bean for reverse request

--- Spring_Gateway/src/main/java/com/example/spring_gateway/config/SpringCloudConfig.java
+++ Spring_Gateway/src/main/java/com/example/spring_gateway/config/SpringCloudConfig.java
@@ -1,4 +1,18 @@
 package com.example.spring_gateway.config;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SpringCloudConfig {
+
+    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/test/**")
+                        .uri("http://localhost:8085/"))
+                .build();
+    }
 }
