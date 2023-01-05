e864bde86086cead78eba797e4e53aea2bbfb604
farzadafi <farzadafi40@gmail.com>
2023-01-05 05:41:41 +0330

    create server_test module

---
+++ server_test/src/main/java/com/example/server_test/ServerTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.server_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerTestApplication.class, args);
+    }
+
+}
---
+++ server_test/src/test/java/com/example/server_test/ServerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.server_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
