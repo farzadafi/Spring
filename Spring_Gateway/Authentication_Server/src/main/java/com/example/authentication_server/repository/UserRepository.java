4dd77a2ba776f337bfd19d11d0b889cf73da0ee7
farzadafi <farzadafi40@gmail.com>
2023-01-08 13:50:37 +0330

    extend from JpaRepository

--- Spring_Gateway/Authentication_Server/src/main/java/com/example/authentication_server/repository/UserRepository.java
+++ Spring_Gateway/Authentication_Server/src/main/java/com/example/authentication_server/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.example.authentication_server.repository;
 
-public interface UserRepository {
+import com.example.authentication_server.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
