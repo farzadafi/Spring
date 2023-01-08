47dd020ce8bc5b3dc2d359e1ec74f4eeb1980408
farzadafi <farzadafi40@gmail.com>
2023-01-08 14:00:17 +0330

    add new constructor

--- Spring_Gateway/Authentication_Server/src/main/java/com/example/authentication_server/exception/UserNameDuplicateException.java
+++ Spring_Gateway/Authentication_Server/src/main/java/com/example/authentication_server/exception/UserNameDuplicateException.java
@@ -1,4 +1,8 @@
 package com.example.authentication_server.exception;
 
-public class UserNameDuplicateException {
+public class UserNameDuplicateException extends RuntimeException{
+
+    public UserNameDuplicateException(String message) {
+        super(message);
+    }
 }
