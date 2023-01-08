6d744b3b7e03bb1c632714daa8f29ec1026c1e42
farzadafi <farzadafi40@gmail.com>
2023-01-08 14:10:01 +0330

    create UserMapper interface

---
+++ Spring_Gateway/Authentication_Server/src/main/java/com/example/authentication_server/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.example.authentication_server.mapper;
+
+import com.example.authentication_server.dto.UserDto;
+import com.example.authentication_server.model.User;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+}
