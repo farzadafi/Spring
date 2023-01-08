b4cc9e616c5f920f7dedfa45a0fe15bda0fc03ba
farzadafi <farzadafi40@gmail.com>
2023-01-08 13:37:54 +0330

    add some needed field

--- Spring_Gateway/Authentication_Server/src/main/java/com/example/authentication_server/model/User.java
+++ Spring_Gateway/Authentication_Server/src/main/java/com/example/authentication_server/model/User.java
@@ -1,4 +1,18 @@
 package com.example.authentication_server.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue
+    Integer id;
+
+    String fullName;
+
+    @Column(unique = true)
+    String username;
+
+    String password;
 }
