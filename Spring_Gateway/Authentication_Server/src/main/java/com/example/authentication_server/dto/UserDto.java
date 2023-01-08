43f2d2b22045a00638c09851750459be5cbe80f6
farzadafi <farzadafi40@gmail.com>
2023-01-08 14:06:51 +0330

    add some needed field

--- Spring_Gateway/Authentication_Server/src/main/java/com/example/authentication_server/dto/UserDto.java
+++ Spring_Gateway/Authentication_Server/src/main/java/com/example/authentication_server/dto/UserDto.java
@@ -1,4 +1,42 @@
 package com.example.authentication_server.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserDto {
+    Integer id;
+
+    String fullName;
+
+    @NotNull
+    @Pattern(regexp = "^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$",
+            message = "please enter a valid user name(just alphabetic or numeric char)")
+    private String username;
+
+    @NotNull
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@$!%*?&#^])[A-Za-z0-9@$!%*?&]{8,10}$",
+            message = "password must :Minimum 8, maximum 10 char, at least one uppercase, one lowercase, one number and one (!@#$%^&*) ")
+    private String password;
+
+
+/*
+    regex pattern for user name
+^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$
+ └─────┬────┘└───┬──┘└─────┬─────┘└─────┬─────┘ └───┬───┘
+       │         │         │            │           no _ or . at the end
+       │         │         │            │
+       │         │         │            allowed characters
+       │         │         │
+       │         │         no __ or _. or ._ or .. inside
+       │         │
+       │         no _ or . at the beginning
+       │
+       username is 8-20 characters long*/
 }
