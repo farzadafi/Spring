aeed8841b75db4721ff5339155e2e5b4f1c1bdeb
farzadafi <farzadafi40@gmail.com>
2023-01-08 14:03:50 +0330

    add findByUsername method

--- Spring_Gateway/Authentication_Server/src/main/java/com/example/authentication_server/repository/UserRepository.java
+++ Spring_Gateway/Authentication_Server/src/main/java/com/example/authentication_server/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.example.authentication_server.repository;
 import com.example.authentication_server.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
 }
