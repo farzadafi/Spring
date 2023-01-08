6977b2154baaabe70468aa7ec48c4615f8844fa6
farzadafi <farzadafi40@gmail.com>
2023-01-08 13:38:11 +0330

    add lombok annotation

--- Spring_Gateway/Authentication_Server/src/main/java/com/example/authentication_server/model/User.java
+++ Spring_Gateway/Authentication_Server/src/main/java/com/example/authentication_server/model/User.java
@@ -1,8 +1,15 @@
 package com.example.authentication_server.model;
 
 import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 @Entity
+@FieldDefaults(level = AccessLevel.PRIVATE)
 @Table(name = "users")
 public class User {
     @Id
