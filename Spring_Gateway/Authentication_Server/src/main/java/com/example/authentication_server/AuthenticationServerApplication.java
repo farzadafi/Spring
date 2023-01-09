6e07787aa2b3d67716ab713ae0fcaa7aee9d04b3
farzadafi <farzadafi40@gmail.com>
2023-01-09 18:10:16 +0330

    create AuthenticationServerApplication.java

---
+++ Spring_Gateway/Authentication_Server/src/main/java/com/example/authentication_server/AuthenticationServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.authentication_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationServerApplication.class, args);
+    }
+
+}
