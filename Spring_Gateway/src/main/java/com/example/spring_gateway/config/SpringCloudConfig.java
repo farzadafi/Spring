f58a50b926f15442b8e242facc732a9696405e29
farzadafi <farzadafi40@gmail.com>
2023-01-09 18:06:19 +0330

    add new route

--- Spring_Gateway/src/main/java/com/example/spring_gateway/config/SpringCloudConfig.java
+++ Spring_Gateway/src/main/java/com/example/spring_gateway/config/SpringCloudConfig.java
@@ -1,5 +1,6 @@
 package com.example.spring_gateway.config;
 
+import com.example.spring_gateway.filter.AuthenticationFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -8,11 +9,20 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringCloudConfig {
 
+    private final AuthenticationFilter authenticationFilter;
+
+    public SpringCloudConfig(AuthenticationFilter authenticationFilter) {
+        this.authenticationFilter = authenticationFilter;
+    }
+
     @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(r -> r.path("/test/**")
+                        //.filters(f -> f.filter(authenticationFilter).stripPrefix(1))
                         .uri("http://localhost:8085/"))
+                .route(r -> r.path("/user")
+                        .uri("http://localhost:8082"))
                 .build();
     }
 }
