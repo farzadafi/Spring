715c7d4dee960f09e515d344ab26672c9af9d180
farzadafi <farzadafi40@gmail.com>
2023-01-09 18:26:39 +0330

    add @ResponseStatus

--- Spring_Gateway/Authentication_Server/src/main/java/com/example/authentication_server/exception/UserNotFoundException.java
+++ Spring_Gateway/Authentication_Server/src/main/java/com/example/authentication_server/exception/UserNotFoundException.java
@@ -1,5 +1,9 @@
 package com.example.authentication_server.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "username not found")
 public class UserNotFoundException extends RuntimeException{
 
     public UserNotFoundException(String message) {
