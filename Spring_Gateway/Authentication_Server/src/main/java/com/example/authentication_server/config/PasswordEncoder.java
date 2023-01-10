b6addeb4a79f8e875149fe9dbe47d176fef12273
farzadafi <farzadafi40@gmail.com>
2023-01-10 16:50:55 +0330

    add bCryptPasswordEncoder method

--- Spring_Gateway/Authentication_Server/src/main/java/com/example/authentication_server/config/PasswordEncoder.java
+++ Spring_Gateway/Authentication_Server/src/main/java/com/example/authentication_server/config/PasswordEncoder.java
@@ -1,4 +1,14 @@
 package com.example.authentication_server.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
 public class PasswordEncoder {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
