ad35d37bd6fe52386b6cee5ad18927d5b284c0b0
farzadafi <farzadafi40@gmail.com>
2023-01-10 16:41:53 +0330

    fix createToken method

--- Spring_Gateway/Authentication_Server/src/main/java/com/example/authentication_server/jwt/Jwt.java
+++ Spring_Gateway/Authentication_Server/src/main/java/com/example/authentication_server/jwt/Jwt.java
@@ -1,4 +1,33 @@
 package com.example.authentication_server.jwt;
 
+import com.example.authentication_server.service.user.UserServiceImpel;
+import io.jsonwebtoken.Jwts;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.time.LocalDate;
+import java.util.Date;
+
+@Component
 public class Jwt {
+
+    private final SecretKey secretKey;
+    private final UserServiceImpel userServiceImpel;
+
+    public Jwt(SecretKey secretKey, UserServiceImpel userServiceImpel) {
+        this.secretKey = secretKey;
+        this.userServiceImpel = userServiceImpel;
+    }
+
+    public String createToken(String username, String password) {
+        if (userServiceImpel.isLogin(username, password)) {
+            return Jwts.builder()
+                    .setSubject(username)
+                    .setIssuedAt(new Date())
+                    .setExpiration(java.sql.Date.valueOf(LocalDate.now().plusDays(14)))
+                    .signWith(secretKey)
+                    .compact();
+        }
+        return null;
+    }
 }
