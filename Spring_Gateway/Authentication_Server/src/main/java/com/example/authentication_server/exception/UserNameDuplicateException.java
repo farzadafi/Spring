cffa4538b71a4bc8b2248ea46bd4ff1416c8b27b
farzadafi <farzadafi40@gmail.com>
2023-01-10 16:24:36 +0330

    remove @ResponseStatus

--- Spring_Gateway/Authentication_Server/src/main/java/com/example/authentication_server/exception/UserNameDuplicateException.java
+++ Spring_Gateway/Authentication_Server/src/main/java/com/example/authentication_server/exception/UserNameDuplicateException.java
@@ -1,9 +1,5 @@
 package com.example.authentication_server.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "This username already exists")
 public class UserNameDuplicateException extends RuntimeException{
 
     public UserNameDuplicateException(String message) {
