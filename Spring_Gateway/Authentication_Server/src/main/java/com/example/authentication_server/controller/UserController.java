65c594372ffffc762bdacb030c4aa93e414adc57
farzadafi <farzadafi40@gmail.com>
2023-01-10 16:42:50 +0330

    fix getToken method

--- Spring_Gateway/Authentication_Server/src/main/java/com/example/authentication_server/controller/UserController.java
+++ Spring_Gateway/Authentication_Server/src/main/java/com/example/authentication_server/controller/UserController.java
@@ -1,23 +1,23 @@
 package com.example.authentication_server.controller;
 
 import com.example.authentication_server.dto.UserDto;
+import com.example.authentication_server.jwt.Jwt;
 import com.example.authentication_server.mapper.UserMapper;
 import com.example.authentication_server.model.User;
 import com.example.authentication_server.service.user.UserServiceImpel;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
     private final UserServiceImpel userServiceImpel;
+    private final Jwt jwt;
 
-    public UserController(UserServiceImpel userServiceImpel) {
+    public UserController(UserServiceImpel userServiceImpel, Jwt jwt) {
         this.userServiceImpel = userServiceImpel;
+        this.jwt = jwt;
     }
 
     @PostMapping("/register")
@@ -25,5 +25,10 @@ public class UserController {
         User user = UserMapper.INSTANCE.dtoToModel(userDto);
         userServiceImpel.registerUser(user);
         return "OK";
+    }
+
+    @GetMapping("/getToken/{username}/{password}")
+    public String getToken(@PathVariable String username, @PathVariable String password) {
+        return jwt.createToken(username, password);
     }
 }
