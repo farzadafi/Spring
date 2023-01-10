58a9ae011ec6be7365099e603a23fbfc8c4a4b83
farzadafi <farzadafi40@gmail.com>
2023-01-10 16:30:46 +0330

    create JwtSecretKey class

---
+++ Spring_Gateway/Authentication_Server/src/main/java/com/example/authentication_server/jwt/JwtSecretKey.java
@@ -0,0 +1,14 @@
+package com.example.authentication_server.jwt;
+
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.crypto.SecretKey;
+
+@Configuration
+public class JwtSecretKey {
+
+
+}
