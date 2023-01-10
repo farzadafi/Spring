df31a8499e9f13dbdd7cecc29a32831add6f7a25
farzadafi <farzadafi40@gmail.com>
2023-01-10 16:20:24 +0330

    create GlobalExceptionHandler and add @ControllerAdvice

---
+++ Spring_Gateway/Authentication_Server/src/main/java/com/example/authentication_server/exception/GlobalExceptionHandler.java
@@ -0,0 +1,12 @@
+package com.example.authentication_server.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+
+}
