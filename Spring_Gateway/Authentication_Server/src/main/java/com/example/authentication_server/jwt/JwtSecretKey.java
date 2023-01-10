208b977a5fd2a35ca54ecf59bb1cd728a332bff9
farzadafi <farzadafi40@gmail.com>
2023-01-10 16:31:05 +0330

    add secretKey method

--- Spring_Gateway/Authentication_Server/src/main/java/com/example/authentication_server/jwt/JwtSecretKey.java
+++ Spring_Gateway/Authentication_Server/src/main/java/com/example/authentication_server/jwt/JwtSecretKey.java
@@ -1,7 +1,6 @@
 package com.example.authentication_server.jwt;
 
 import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,5 +9,10 @@ import javax.crypto.SecretKey;
 @Configuration
 public class JwtSecretKey {
 
-
+    @Bean
+    public SecretKey secretKey() {
+        String SECRET_KEY =
+                "YzMzN2UwY2JkY23NGYxNWFiNmVkNTczM44TBY2ZlZmQwNTk2ZDUyNG1RN9WQ0ZmU3ODhjOTBljUxMzIyYzg0Mg==";
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    }
 }
