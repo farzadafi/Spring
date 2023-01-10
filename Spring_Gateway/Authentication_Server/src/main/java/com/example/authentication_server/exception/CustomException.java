2045b6f438d817ed5b5eab068a7b2a8b378836c9
farzadafi <farzadafi40@gmail.com>
2023-01-10 16:30:10 +0330

    remove @Getter and @AllArgsConstructor

--- Spring_Gateway/Authentication_Server/src/main/java/com/example/authentication_server/exception/CustomException.java
+++ Spring_Gateway/Authentication_Server/src/main/java/com/example/authentication_server/exception/CustomException.java
@@ -1,10 +1,6 @@
 package com.example.authentication_server.exception;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
-@Getter
-@AllArgsConstructor
 public record CustomException(HttpStatus httpStatus, String message) {
 }
