8aa94b12cdd4e002d6793280d8f00cecb7729f38
farzadafi <farzadafi40@gmail.com>
2023-01-11 20:17:01 +0330

    new comit from terminal for test

--- Spring_Security/src/main/java/com/example/spring_security/SpringSecurityApplication.java
+++ Spring_Security/src/main/java/com/example/spring_security/SpringSecurityApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringSecurityApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] argsNew) {
         SpringApplication.run(SpringSecurityApplication.class, args);
     }
 
