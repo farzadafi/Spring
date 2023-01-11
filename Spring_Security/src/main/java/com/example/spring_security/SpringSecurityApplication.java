dc9f5eacff221e028519d169beddae059529254e
farzad <Farzadafi40@gmail.com>
2023-01-11 20:15:18 +0330

    new commit for test

--- Spring_Security/src/main/java/com/example/spring_security/SpringSecurityApplication.java
+++ Spring_Security/src/main/java/com/example/spring_security/SpringSecurityApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringSecurityApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[]) {
         SpringApplication.run(SpringSecurityApplication.class, args);
     }
 
