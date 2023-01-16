e90b399b0a87550457541aaca8640b29e18e67df
farzadafi <farzadafi40@gmail.com>
2023-01-16 19:54:01 +0330

    create one constructor

--- Spring_Gateway/src/main/java/com/example/spring_gateway/exception/InvalidTokenException.java
+++ Spring_Gateway/src/main/java/com/example/spring_gateway/exception/InvalidTokenException.java
@@ -1,4 +1,8 @@
 package com.example.spring_gateway.exception;
 
 public class InvalidTokenException extends RuntimeException{
+
+    public InvalidTokenException(String message) {
+        super(message);
+    }
 }
