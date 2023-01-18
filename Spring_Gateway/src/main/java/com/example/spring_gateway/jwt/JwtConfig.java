4816b441af4e52e394e1ca708e574041898dc1e4
farzadafi <farzadafi40@gmail.com>
2023-01-18 14:13:04 +0330

    add secret key and tokenPrefix field

--- Spring_Gateway/src/main/java/com/example/spring_gateway/jwt/JwtConfig.java
+++ Spring_Gateway/src/main/java/com/example/spring_gateway/jwt/JwtConfig.java
@@ -1,4 +1,13 @@
 package com.example.spring_gateway.jwt;
 
+import lombok.Getter;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
 public class JwtConfig {
+
+    private final String secretKey =
+            "YzMzN2UwY2JkY23NGYxNWFiNmVkNTczM44TBY2ZlZmQwNTk2ZDUyNG1RN9WQ0ZmU3ODhjOTBljUxMzIyYzg0Mg==";
+    private final String tokenPrefix = "Bearer ";
 }
