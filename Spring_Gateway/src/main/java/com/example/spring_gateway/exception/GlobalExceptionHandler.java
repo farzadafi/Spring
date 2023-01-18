93131886c542506d6f4184a6ace2de813103b00e
farzadafi <farzadafi40@gmail.com>
2023-01-18 14:35:18 +0330

    add invalidHeaderExceptionHandler method

--- Spring_Gateway/src/main/java/com/example/spring_gateway/exception/GlobalExceptionHandler.java
+++ Spring_Gateway/src/main/java/com/example/spring_gateway/exception/GlobalExceptionHandler.java
@@ -1,4 +1,17 @@
 package com.example.spring_gateway.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(InvalidHeaderException.class)
+    public ResponseEntity<?> invalidHeaderExceptionHandler(InvalidHeaderException e) {
+
+        CustomException exception = new CustomException(HttpStatus.BAD_REQUEST, e.getLocalizedMessage());
+        return new ResponseEntity<>(exception, exception.httpStatus());
+    }
 }
