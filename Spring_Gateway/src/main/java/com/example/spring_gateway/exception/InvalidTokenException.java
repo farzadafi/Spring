72b88108514b8ac7d1f83054140bc0528a4e82db
farzadafi <farzadafi40@gmail.com>
2023-01-18 15:24:57 +0330

    add @ResponseStatus

--- Spring_Gateway/src/main/java/com/example/spring_gateway/exception/InvalidTokenException.java
+++ Spring_Gateway/src/main/java/com/example/spring_gateway/exception/InvalidTokenException.java
@@ -1,5 +1,9 @@
 package com.example.spring_gateway.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.FORBIDDEN, reason = "Token is invalid")
 public class InvalidTokenException extends RuntimeException{
 
     public InvalidTokenException(String message) {
