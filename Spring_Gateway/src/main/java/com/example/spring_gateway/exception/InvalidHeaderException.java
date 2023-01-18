ec5767e1f47703c6cf0c113b7433a0b9b4df12c0
farzadafi <farzadafi40@gmail.com>
2023-01-18 15:22:52 +0330

    add @ResponseStatus

--- Spring_Gateway/src/main/java/com/example/spring_gateway/exception/InvalidHeaderException.java
+++ Spring_Gateway/src/main/java/com/example/spring_gateway/exception/InvalidHeaderException.java
@@ -1,5 +1,9 @@
 package com.example.spring_gateway.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST,reason = "Please send a Bearer token")
 public class InvalidHeaderException extends RuntimeException{
 
     public InvalidHeaderException(String message) {
