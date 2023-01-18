fa7e4131cb9702941396efa3fa9f21e15f2cdf54
farzadafi <farzadafi40@gmail.com>
2023-01-18 14:32:24 +0330

    add httpStatus and message field

--- Spring_Gateway/src/main/java/com/example/spring_gateway/exception/CustomException.java
+++ Spring_Gateway/src/main/java/com/example/spring_gateway/exception/CustomException.java
@@ -1,4 +1,6 @@
 package com.example.spring_gateway.exception;
 
-public record CustomException() {
+import org.springframework.http.HttpStatus;
+
+public record CustomException(HttpStatus httpStatus, String message) {
 }
