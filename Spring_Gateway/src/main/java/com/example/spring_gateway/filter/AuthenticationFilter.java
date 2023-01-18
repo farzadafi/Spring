cadb652c82d229d1b247d681a9bbec4e7c039c40
farzadafi <farzadafi40@gmail.com>
2023-01-18 15:28:24 +0330

    extend from GatewayFilter and override filter method

--- Spring_Gateway/src/main/java/com/example/spring_gateway/filter/AuthenticationFilter.java
+++ Spring_Gateway/src/main/java/com/example/spring_gateway/filter/AuthenticationFilter.java
@@ -1,4 +1,26 @@
 package com.example.spring_gateway.filter;
 
-public class AuthenticationFilter {
+import com.example.spring_gateway.jwt.JwtTokenVerifier;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+public class AuthenticationFilter implements GatewayFilter {
+
+    private final JwtTokenVerifier tokenVerifier;
+
+    public AuthenticationFilter(JwtTokenVerifier tokenVerifier) {
+        this.tokenVerifier = tokenVerifier;
+    }
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        tokenVerifier.tokenVerifier(request);
+        return chain.filter(exchange);
+    }
 }
