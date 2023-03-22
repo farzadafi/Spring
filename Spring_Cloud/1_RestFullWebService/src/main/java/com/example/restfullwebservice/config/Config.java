0dc9567ad0ee6e34711bc81235b983922f07b449
farzadafi <farzadafi40@gmail.com>
2023-03-22 08:53:46 +0330

    add new Bean for model mapper

--- Spring_Cloud/1_RestFullWebService/src/main/java/com/example/restfullwebservice/config/Config.java
+++ Spring_Cloud/1_RestFullWebService/src/main/java/com/example/restfullwebservice/config/Config.java
@@ -1,4 +1,14 @@
 package com.example.restfullwebservice.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class Config {
+
+    @Bean
+    public ModelMapper mapper() {
+        return new ModelMapper();
+    }
 }
