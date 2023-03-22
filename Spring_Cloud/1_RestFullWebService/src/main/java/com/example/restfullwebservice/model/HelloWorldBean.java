84a38334077ce60f94601d6f6aaed82fa3f6a34f
farzadafi <farzadafi40@gmail.com>
2023-03-22 06:46:51 +0330

    add some lombok annotation

--- Spring_Cloud/1_RestFullWebService/src/main/java/com/example/restfullwebservice/model/HelloWorldBean.java
+++ Spring_Cloud/1_RestFullWebService/src/main/java/com/example/restfullwebservice/model/HelloWorldBean.java
@@ -1,10 +1,15 @@
 package com.example.restfullwebservice.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class HelloWorldBean {
 
     private String message;
-
-    public HelloWorldBean(String message) {
-        this.message = message;
-    }
 }
