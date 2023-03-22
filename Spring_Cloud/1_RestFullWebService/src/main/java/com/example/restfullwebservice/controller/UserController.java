80b181db72a70d8e4dc4274e1a454875e1598c51
farzadafi <farzadafi40@gmail.com>
2023-03-22 07:02:27 +0330

    add retrieveAllUser method

---
+++ Spring_Cloud/1_RestFullWebService/src/main/java/com/example/restfullwebservice/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.example.restfullwebservice.controller;
+
+import com.example.restfullwebservice.model.User;
+import com.example.restfullwebservice.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    public final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUser() {
+        return userService.findAll();
+    }
+}
