58eae6f74ae24f6473ed287d895ab91fe3613e86
farzadafi <farzadafi40@gmail.com>
2023-03-22 06:56:25 +0330

    add three field for user

--- Spring_Cloud/1_RestFullWebService/src/main/java/com/example/restfullwebservice/model/User.java
+++ Spring_Cloud/1_RestFullWebService/src/main/java/com/example/restfullwebservice/model/User.java
@@ -1,4 +1,17 @@
 package com.example.restfullwebservice.model;
 
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@ToString
 public class User {
+    private Integer id;
+    private String name;
+    private LocalDateTime birthdate;
 }
