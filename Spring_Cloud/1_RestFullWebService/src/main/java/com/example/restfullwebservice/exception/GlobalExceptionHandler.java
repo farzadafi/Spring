74a069c15a9e77da75f2537d17d9f3680ec7d1bd
farzadafi <farzadafi40@gmail.com>
2023-03-22 09:18:26 +0330

    extend from ResponseEntityExceptionHandler class

--- Spring_Cloud/1_RestFullWebService/src/main/java/com/example/restfullwebservice/exception/GlobalExceptionHandler.java
+++ Spring_Cloud/1_RestFullWebService/src/main/java/com/example/restfullwebservice/exception/GlobalExceptionHandler.java
@@ -5,11 +5,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<ErrorDetails> userNotFoundExceptionHandler(UserNotFoundException e) {
