f3bb08707f959596d9dffbc74c38de2ea0486ab4
farzadafi <farzadafi40@gmail.com>
2023-03-22 08:22:34 +0330

    create ErrorDetails class

---
+++ Spring_Cloud/1_RestFullWebService/src/main/java/com/example/restfullwebservice/dto/ErrorDetails.java
@@ -0,0 +1,19 @@
+package com.example.restfullwebservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDetails {
+
+    private LocalDateTime localDateTime;
+    private String message;
+    private String details;
+}
