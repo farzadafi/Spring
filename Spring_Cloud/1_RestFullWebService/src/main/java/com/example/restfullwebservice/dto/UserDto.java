34df2ee12a0075c13585469fcc1ab9c10c2ccc01
farzadafi <farzadafi40@gmail.com>
2023-03-22 08:50:55 +0330

    create UserDto class

---
+++ Spring_Cloud/1_RestFullWebService/src/main/java/com/example/restfullwebservice/dto/UserDto.java
@@ -0,0 +1,18 @@
+package com.example.restfullwebservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    private int id;
+    private String name;
+    private LocalDateTime birthdate;
+}
