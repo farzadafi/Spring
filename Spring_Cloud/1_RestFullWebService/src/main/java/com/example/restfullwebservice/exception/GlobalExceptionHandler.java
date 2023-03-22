a5b561b69d17ceb2f13dabbc646f86a614d8c46a
farzadafi <farzadafi40@gmail.com>
2023-03-22 08:32:35 +0330

    add userNotFoundExceptionHandler method

--- Spring_Cloud/1_RestFullWebService/src/main/java/com/example/restfullwebservice/exception/GlobalExceptionHandler.java
+++ Spring_Cloud/1_RestFullWebService/src/main/java/com/example/restfullwebservice/exception/GlobalExceptionHandler.java
@@ -1,4 +1,19 @@
 package com.example.restfullwebservice.exception;
 
+import com.example.restfullwebservice.dto.ErrorDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorDetails> userNotFoundExceptionHandler(UserNotFoundException e) {
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), e.getMessage(), 404);
+        return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
 }
