899f73f9f9edee49e34280f95d44292d08f1d9f0
farzadafi <farzadafi40@gmail.com>
2023-03-22 07:07:47 +0330

    extend from RuntimeException and add new constructor

--- Spring_Cloud/1_RestFullWebService/src/main/java/com/example/restfullwebservice/exception/UserNotFoundException.java
+++ Spring_Cloud/1_RestFullWebService/src/main/java/com/example/restfullwebservice/exception/UserNotFoundException.java
@@ -1,4 +1,12 @@
 package com.example.restfullwebservice.exception;
 
-public class UserNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class UserNotFoundException extends RuntimeException{
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
 }
