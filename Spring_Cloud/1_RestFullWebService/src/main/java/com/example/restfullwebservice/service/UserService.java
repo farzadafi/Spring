4df716e0694e71099a81d413f6c1d1a5f6ed85c6
farzadafi <farzadafi40@gmail.com>
2023-03-22 07:08:47 +0330

    add findById method

--- Spring_Cloud/1_RestFullWebService/src/main/java/com/example/restfullwebservice/service/UserService.java
+++ Spring_Cloud/1_RestFullWebService/src/main/java/com/example/restfullwebservice/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.restfullwebservice.service;
 
+import com.example.restfullwebservice.exception.UserNotFoundException;
 import com.example.restfullwebservice.model.User;
 import org.springframework.stereotype.Component;
 
@@ -13,12 +14,21 @@ public class UserService {
     private static final List<User> users = new ArrayList<>();
 
     static {
-        users.add(new User(1, "farzad",  LocalDateTime.now().minusYears(26)));
-        users.add(new User(1, "farhad",  LocalDateTime.now().minusYears(25)));
-        users.add(new User(1, "fardad",  LocalDateTime.now().minusYears(24)));
+        users.add(new User(1, "farzad", LocalDateTime.now().minusYears(26)));
+        users.add(new User(2, "farhad", LocalDateTime.now().minusYears(25)));
+        users.add(new User(3, "fardad", LocalDateTime.now().minusYears(24)));
     }
 
     public List<User> findAll() {
         return users;
+    }
+
+    public User findById(Integer id) {
+        return users.stream().
+                filter(user -> user
+                        .getId()
+                        .equals(id))
+                .findFirst()
+                .orElseThrow(() -> new UserNotFoundException("this user not found"));
     }
 }
