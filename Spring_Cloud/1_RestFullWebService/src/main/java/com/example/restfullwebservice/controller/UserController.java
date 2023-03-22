77b8029fd4f2b1f11f1ad3943c97a373051e91fb
farzadafi <farzadafi40@gmail.com>
2023-03-22 07:03:14 +0330

    add @RequestMapping

--- Spring_Cloud/1_RestFullWebService/src/main/java/com/example/restfullwebservice/controller/UserController.java
+++ Spring_Cloud/1_RestFullWebService/src/main/java/com/example/restfullwebservice/controller/UserController.java
@@ -3,11 +3,13 @@ package com.example.restfullwebservice.controller;
 import com.example.restfullwebservice.model.User;
 import com.example.restfullwebservice.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
     public final UserService userService;
