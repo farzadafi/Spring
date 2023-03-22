3c51d895087ddbd64c4471a6a72aaed6c21249f0
farzadafi <farzadafi40@gmail.com>
2023-03-22 06:08:47 +0330

    create 1_RestFullWebService module

---
+++ Spring_Cloud/1_RestFullWebService/src/main/java/com/example/restfullwebservice/RestFullWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfullwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFullWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestFullWebServiceApplication.class, args);
+    }
+
+}
---
+++ Spring_Cloud/1_RestFullWebService/src/test/java/com/example/restfullwebservice/RestFullWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restfullwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestFullWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
