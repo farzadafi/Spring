e07f48c1029b1ae687a074ad58b262977a552b29
farzadafi <farzadafi40@gmail.com>
2023-03-22 08:30:44 +0330

    comment @ResponseStatus

--- Spring_Cloud/1_RestFullWebService/src/main/java/com/example/restfullwebservice/exception/UserNotFoundException.java
+++ Spring_Cloud/1_RestFullWebService/src/main/java/com/example/restfullwebservice/exception/UserNotFoundException.java
@@ -1,9 +1,6 @@
 package com.example.restfullwebservice.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
+//@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "this user not found!")
 public class UserNotFoundException extends RuntimeException{
 
     public UserNotFoundException(String message) {
