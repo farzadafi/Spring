04abf8cf53f8e161c7332e02ac5270d1d2ec6fcd
farzadafi <farzadafi40@gmail.com>
2023-03-24 20:37:32 +0330

    add goodMorning method

--- Spring_Cloud/1_RestFullWebService/src/main/java/com/example/restfullwebservice/controller/HelloWorldController.java
+++ Spring_Cloud/1_RestFullWebService/src/main/java/com/example/restfullwebservice/controller/HelloWorldController.java
@@ -1,16 +1,33 @@
 package com.example.restfullwebservice.controller;
 
 import com.example.restfullwebservice.model.HelloWorldBean;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 @RequestMapping("/hello-world")
 public class HelloWorldController {
 
+    private final MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
     @GetMapping("hello")
     public HelloWorldBean helloWorld() {
         return new HelloWorldBean("hello world");
+    }
+
+    @GetMapping("hello")
+    public HelloWorldBean goodMorning() {
+        Locale locale = LocaleContextHolder.getLocale();
+        String message = messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+        return new HelloWorldBean(message);
     }
 }
