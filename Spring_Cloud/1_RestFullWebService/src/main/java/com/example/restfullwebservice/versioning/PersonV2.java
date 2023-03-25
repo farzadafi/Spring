5ac98f64cf844c8213096544a799e839645544b5
farzadafi <farzadafi40@gmail.com>
2023-03-25 15:30:53 +0330

    add needed field and add lombok annotation

--- Spring_Cloud/1_RestFullWebService/src/main/java/com/example/restfullwebservice/versioning/PersonV2.java
+++ Spring_Cloud/1_RestFullWebService/src/main/java/com/example/restfullwebservice/versioning/PersonV2.java
@@ -1,4 +1,15 @@
 package com.example.restfullwebservice.versioning;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class PersonV2 {
+    private String firstName;
+    private String lastName;
 }
