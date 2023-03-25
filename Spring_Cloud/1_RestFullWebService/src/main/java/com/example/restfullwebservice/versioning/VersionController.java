0aebb5df0219758fd70c55efca3943f83f1bfe09
farzadafi <farzadafi40@gmail.com>
2023-03-25 15:32:38 +0330

    add getPersonV1 method

--- Spring_Cloud/1_RestFullWebService/src/main/java/com/example/restfullwebservice/versioning/VersionController.java
+++ Spring_Cloud/1_RestFullWebService/src/main/java/com/example/restfullwebservice/versioning/VersionController.java
@@ -1,4 +1,13 @@
 package com.example.restfullwebservice.versioning;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class VersionController {
+
+    @GetMapping("/v1/person")
+    public PersonV1 getPersonV1() {
+        return new PersonV1("Farzad Afshar");
+    }
 }
