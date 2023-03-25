dfc133c4c713a11b9852dff4fd8e42cf11187d53
farzadafi <farzadafi40@gmail.com>
2023-03-25 15:33:22 +0330

    add getPersonV2 method

--- Spring_Cloud/1_RestFullWebService/src/main/java/com/example/restfullwebservice/versioning/VersionController.java
+++ Spring_Cloud/1_RestFullWebService/src/main/java/com/example/restfullwebservice/versioning/VersionController.java
@@ -10,4 +10,9 @@ public class VersionController {
     public PersonV1 getPersonV1() {
         return new PersonV1("Farzad Afshar");
     }
+
+    @GetMapping("/v1/person")
+    public PersonV2 getPersonV2() {
+        return new PersonV2("Farzad", "Afshar");
+    }
 }
