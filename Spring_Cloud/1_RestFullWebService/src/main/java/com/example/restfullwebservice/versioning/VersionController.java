634f59cb02c707c783e8f11fb1fd38acf0373157
farzadafi <farzadafi40@gmail.com>
2023-03-25 16:45:58 +0330

    add two method based on request param

--- Spring_Cloud/1_RestFullWebService/src/main/java/com/example/restfullwebservice/versioning/VersionController.java
+++ Spring_Cloud/1_RestFullWebService/src/main/java/com/example/restfullwebservice/versioning/VersionController.java
@@ -6,13 +6,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class VersionController {
 
+    //based on url
     @GetMapping("/v1/person")
-    public PersonV1 getPersonV1() {
+    public PersonV1 getPersonV1BasedOnUrl() {
         return new PersonV1("Farzad Afshar");
     }
 
     @GetMapping("/v1/person")
-    public PersonV2 getPersonV2() {
+    public PersonV2 getPersonV2BasedOnUrl() {
+        return new PersonV2("Farzad", "Afshar");
+    }
+
+    //****
+    //based on Request param
+    @GetMapping(path = "/person", params = "version=1")
+    public PersonV1 getPersonV1BasedOnRequestParam() {
+        return new PersonV1("Farzad Afshar");
+    }
+
+    @GetMapping(path = "/person", params = "version=2")
+    public PersonV2 getPersonV2BasedOnRequestParam() {
         return new PersonV2("Farzad", "Afshar");
     }
 }
