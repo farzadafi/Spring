a77b80b212f8555d0e59ec17b9dcdb9daa436e59
farzadafi <farzadafi40@gmail.com>
2023-03-26 20:45:30 +0330

    create 2_MicroService_SpringCloud module

---
+++ Spring_Cloud/2_MicroService_SpringCloud/src/test/java/com/example/B2_microservice_springcloud/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.B2_microservice_springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
