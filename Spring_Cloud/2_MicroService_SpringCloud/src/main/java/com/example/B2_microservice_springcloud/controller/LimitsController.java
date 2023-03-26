69a08e34a814c36129aef3672e946442be0cd2d3
farzadafi <farzadafi40@gmail.com>
2023-03-26 21:21:29 +0330

    add returnLimits method

--- Spring_Cloud/2_MicroService_SpringCloud/src/main/java/com/example/B2_microservice_springcloud/controller/LimitsController.java
+++ Spring_Cloud/2_MicroService_SpringCloud/src/main/java/com/example/B2_microservice_springcloud/controller/LimitsController.java
@@ -1,4 +1,21 @@
 package com.example.B2_microservice_springcloud.controller;
 
+import com.example.B2_microservice_springcloud.configuration.Config;
+import com.example.B2_microservice_springcloud.model.Limits;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class LimitsController {
+
+    private final Config config;
+
+    public LimitsController(Config config) {
+        this.config = config;
+    }
+
+    @GetMapping("/limits")
+    public Limits returnLimits() {
+        return new Limits(config.getMinimum(), config.getMaximum());
+    }
 }
