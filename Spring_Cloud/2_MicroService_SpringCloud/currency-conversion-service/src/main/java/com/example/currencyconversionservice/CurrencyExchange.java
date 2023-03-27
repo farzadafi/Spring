c2948914f7cda918d7d35a2796e89f4fbfa338aa
farzadafi <farzadafi40@gmail.com>
2023-03-27 18:17:27 +0330

    create CurrencyExchange class

---
+++ Spring_Cloud/2_MicroService_SpringCloud/currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyExchange.java
@@ -0,0 +1,21 @@
+package com.example.currencyconversionservice;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CurrencyExchange {
+
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionMultiple;
+    private String environment;
+}
