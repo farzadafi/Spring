983916b37c30b20e2fa35669fc751360ae4c2361
farzadafi <farzadafi40@gmail.com>
2023-03-27 19:38:23 +0330

    add getExchange method

--- Spring_Cloud/2_MicroService_SpringCloud/currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyExchangeProxy.java
+++ Spring_Cloud/2_MicroService_SpringCloud/currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyExchangeProxy.java
@@ -1,4 +1,12 @@
 package com.example.currencyconversionservice;
 
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
 public interface CurrencyExchangeProxy {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    CurrencyExchange getExchange(@PathVariable String from, @PathVariable String to);
 }
