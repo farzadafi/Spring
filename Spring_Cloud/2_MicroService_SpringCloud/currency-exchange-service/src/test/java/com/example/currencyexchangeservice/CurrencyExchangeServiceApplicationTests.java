71530e35c1382ab897ccfdafc23cfbc3a8395fe9
farzadafi <farzadafi40@gmail.com>
2023-03-27 15:49:27 +0330

    create currency-exchange-service module

---
+++ Spring_Cloud/2_MicroService_SpringCloud/currency-exchange-service/src/test/java/com/example/currencyexchangeservice/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.currencyexchangeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
