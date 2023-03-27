f754a388652022a0f6f2baa218a1ee96df3ee35f
farzadafi <farzadafi40@gmail.com>
2023-03-27 16:56:11 +0330

    add getCurrencyExchange method

--- Spring_Cloud/2_MicroService_SpringCloud/currency-exchange-service/src/main/java/com/example/currencyexchangeservice/CurrencyExchangeController.java
+++ Spring_Cloud/2_MicroService_SpringCloud/currency-exchange-service/src/main/java/com/example/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,4 +1,24 @@
 package com.example.currencyexchangeservice;
 
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
 public class CurrencyExchangeController {
+
+    private final Environment environment;
+
+    public CurrencyExchangeController(Environment environment) {
+        this.environment = environment;
+    }
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange getCurrencyExchange(@PathVariable String from, @PathVariable String to) {
+        String port = environment.getProperty("local.server.port");
+        return new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(500000),port);
+    }
 }
