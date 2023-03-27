5d883403ba4056fbe688164fc7019b58d37e7cc5
farzadafi <farzadafi40@gmail.com>
2023-03-27 16:43:40 +0330

    add lombok annotation and needed field

--- Spring_Cloud/2_MicroService_SpringCloud/currency-exchange-service/src/main/java/com/example/currencyexchangeservice/CurrencyExchange.java
+++ Spring_Cloud/2_MicroService_SpringCloud/currency-exchange-service/src/main/java/com/example/currencyexchangeservice/CurrencyExchange.java
@@ -1,4 +1,20 @@
 package com.example.currencyexchangeservice;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CurrencyExchange {
+
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal conversionMultiple;
 }
