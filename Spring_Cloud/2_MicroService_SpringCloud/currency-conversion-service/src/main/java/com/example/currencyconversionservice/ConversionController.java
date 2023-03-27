615b9416375d1108a4e6d7c5a5f4b4dc5ce37a2e
farzadafi <farzadafi40@gmail.com>
2023-03-27 19:07:50 +0330

    add getExchange method

--- Spring_Cloud/2_MicroService_SpringCloud/currency-conversion-service/src/main/java/com/example/currencyconversionservice/ConversionController.java
+++ Spring_Cloud/2_MicroService_SpringCloud/currency-conversion-service/src/main/java/com/example/currencyconversionservice/ConversionController.java
@@ -1,4 +1,24 @@
 package com.example.currencyconversionservice;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+
+@RestController
 public class ConversionController {
+
+    @GetMapping("/conversion")
+    public CurrencyExchange getExchange() {
+        HashMap<String, String> uriVariable = new HashMap<>();
+        uriVariable.put("from", "Rial");
+        uriVariable.put("to", "dolor");
+
+        ResponseEntity<CurrencyExchange> responseEntity = new RestTemplate().
+                getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+                        CurrencyExchange.class, uriVariable);
+        return responseEntity.getBody();
+    }
 }
