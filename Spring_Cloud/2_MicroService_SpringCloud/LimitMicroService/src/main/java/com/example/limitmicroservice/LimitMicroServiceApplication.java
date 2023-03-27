19ccf48cbd4a0923317496c7135bf9832030889d
farzadafi <farzadafi40@gmail.com>
2023-03-27 13:57:50 +0330

    add LimitsServer modules

---
+++ Spring_Cloud/2_MicroService_SpringCloud/LimitMicroService/src/main/java/com/example/limitmicroservice/LimitMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.limitmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitMicroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LimitMicroServiceApplication.class, args);
+    }
+
+}
