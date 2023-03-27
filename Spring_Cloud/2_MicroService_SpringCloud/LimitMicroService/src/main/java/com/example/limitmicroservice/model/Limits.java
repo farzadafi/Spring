6cbf831ccd4b42980d58e46699ea0e644c561664
farzadafi <farzadafi40@gmail.com>
2023-03-27 13:53:04 +0330

    create Limits class

---
+++ Spring_Cloud/2_MicroService_SpringCloud/LimitMicroService/src/main/java/com/example/limitmicroservice/model/Limits.java
@@ -0,0 +1,15 @@
+package com.example.limitmicroservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Limits {
+    private int minimum;
+    private int maximum;
+}
