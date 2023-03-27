1c5946eb931f0ff36f8c1ab52c587d1836305228
farzadafi <farzadafi40@gmail.com>
2023-03-27 19:45:16 +0330

    add new annotation for feign client config

--- Spring_Cloud/2_MicroService_SpringCloud/currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ Spring_Cloud/2_MicroService_SpringCloud/currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CurrencyConversionServiceApplication {
 
     public static void main(String[] args) {
