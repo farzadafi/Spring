317c86610719d6a7cd293b80d85b4988e2f0aabb
farzadafi <farzadafi40@gmail.com>
2023-03-28 08:21:00 +0330

    create naming-server modules

---
+++ Spring_Cloud/2_MicroService_SpringCloud/naming-server/src/main/java/com/example/namingserver/NamingServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.namingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NamingServerApplication.class, args);
+    }
+
+}
