c2d59f91e51f5246a256b4cfdcd3c0d80798efff
farzadafi <farzadafi40@gmail.com>
2023-03-28 16:40:32 +0330

    add fallback method for when fail retry

--- Spring_Cloud/2_MicroService_SpringCloud/currency-exchange-service/src/main/java/com/example/currencyexchangeservice/RetryController.java
+++ Spring_Cloud/2_MicroService_SpringCloud/currency-exchange-service/src/main/java/com/example/currencyexchangeservice/RetryController.java
@@ -1,13 +1,23 @@
 package com.example.currencyexchangeservice;
 
+import io.github.resilience4j.retry.annotation.Retry;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class RetryController {
 
     @GetMapping("/retry")
+    @Retry(name = "retry", fallbackMethod = "fallback")
     public String retry() {
-        return "Retry method!";
+        ResponseEntity<String> forEntity = new RestTemplate()
+                .getForEntity("http://localhost:1111/naser", String.class);
+        return forEntity.getBody();
+    }
+
+    public String fallback(Exception ex) {
+        return "fallback-response";
     }
 }
