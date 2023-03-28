00ff93168f818f63289642987f6157ba195c3e48
farzadafi <farzadafi40@gmail.com>
2023-03-28 16:26:36 +0330

    add retry method

--- Spring_Cloud/2_MicroService_SpringCloud/currency-exchange-service/src/main/java/com/example/currencyexchangeservice/RetryController.java
+++ Spring_Cloud/2_MicroService_SpringCloud/currency-exchange-service/src/main/java/com/example/currencyexchangeservice/RetryController.java
@@ -1,4 +1,13 @@
 package com.example.currencyexchangeservice;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class RetryController {
+
+    @GetMapping("/retry")
+    public String retry() {
+        return "Retry method!";
+    }
 }
