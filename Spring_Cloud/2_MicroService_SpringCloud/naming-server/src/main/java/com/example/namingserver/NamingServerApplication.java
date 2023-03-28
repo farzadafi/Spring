8353c53cbc2aa0159e7065a9beded250c0c0edf2
farzadafi <farzadafi40@gmail.com>
2023-03-28 08:25:24 +0330

    add eurekaServer annotation

--- Spring_Cloud/2_MicroService_SpringCloud/naming-server/src/main/java/com/example/namingserver/NamingServerApplication.java
+++ Spring_Cloud/2_MicroService_SpringCloud/naming-server/src/main/java/com/example/namingserver/NamingServerApplication.java
@@ -2,7 +2,9 @@ package com.example.namingserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class NamingServerApplication {
 
