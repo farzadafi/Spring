e7e751b6db191dfd105eaa5182eb938e73ee19ff
farzadafi <farzadafi40@gmail.com>
2023-03-28 15:48:08 +0330

    write filter method

--- Spring_Cloud/2_MicroService_SpringCloud/api-gateway/src/main/java/com/example/apigateway/LoggingFilter.java
+++ Spring_Cloud/2_MicroService_SpringCloud/api-gateway/src/main/java/com/example/apigateway/LoggingFilter.java
@@ -1,4 +1,19 @@
 package com.example.apigateway;
 
-public class LoggingFilter {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+public class LoggingFilter implements GlobalFilter {
+
+    private final Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        logger.info("Path of the request received -> {}",exchange.getRequest().getPath());
+        return chain.filter(exchange);
+    }
 }
