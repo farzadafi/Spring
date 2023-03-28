cb1f491d873f4029af405fd62af80923705ca7c6
farzadafi <farzadafi40@gmail.com>
2023-03-28 13:52:04 +0330

    add gatewayRouter method

--- Spring_Cloud/2_MicroService_SpringCloud/api-gateway/src/main/java/com/example/apigateway/ApiGatewayConfiguration.java
+++ Spring_Cloud/2_MicroService_SpringCloud/api-gateway/src/main/java/com/example/apigateway/ApiGatewayConfiguration.java
@@ -1,4 +1,25 @@
 package com.example.apigateway;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ApiGatewayConfiguration {
+
+    @Bean
+    public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(p -> p
+                        .path("/get")
+                        .uri("https://www.google.com"))
+                .route(p -> p
+                        .path("/currency-exchange/**")
+                        .uri("lb://currency-exchange-service"))
+                .route(p -> p
+                        .path("/currency-conversion/**")
+                        .uri("lb://currency-conversion-service"))
+                .build();
+    }
 }
