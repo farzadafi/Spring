54f4eeca30f3dd2f3c12f2f5260b7d069f0ec69b
farzadafi <farzadafi40@gmail.com>
2023-03-28 10:19:43 +0330

    comment url of web service

--- Spring_Cloud/2_MicroService_SpringCloud/currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyExchangeProxy.java
+++ Spring_Cloud/2_MicroService_SpringCloud/currency-conversion-service/src/main/java/com/example/currencyconversionservice/CurrencyExchangeProxy.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange-service", url = "localhost:8000")
+@FeignClient(name = "currency-exchange-service")//, url = "localhost:8000") //comment when use from eureka server
 public interface CurrencyExchangeProxy {
 
     @GetMapping("/currency-exchange/from/{from}/to/{to}")
