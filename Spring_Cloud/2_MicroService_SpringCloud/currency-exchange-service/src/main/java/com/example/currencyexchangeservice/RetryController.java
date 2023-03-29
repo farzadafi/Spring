680c84da66a119cbb3aaacfea26d3ccda15cebb1
farzadafi <farzadafi40@gmail.com>
2023-03-29 07:49:50 +0330

    add some resilience annotation

--- Spring_Cloud/2_MicroService_SpringCloud/currency-exchange-service/src/main/java/com/example/currencyexchangeservice/RetryController.java
+++ Spring_Cloud/2_MicroService_SpringCloud/currency-exchange-service/src/main/java/com/example/currencyexchangeservice/RetryController.java
@@ -1,6 +1,8 @@
 package com.example.currencyexchangeservice;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,8 +13,10 @@ import org.springframework.web.client.RestTemplate;
 public class RetryController {
 
     @GetMapping("/retry")
-    @Retry(name = "retry", fallbackMethod = "fallback")
-    @CircuitBreaker(name = "default", fallbackMethod = "fallback")
+//    @Retry(name = "retry", fallbackMethod = "fallback")
+//    @CircuitBreaker(name = "default", fallbackMethod = "fallback")
+//    @RateLimiter(name = "default")
+//    @Bulkhead(name = "default")
     public String retry() {
         ResponseEntity<String> forEntity = new RestTemplate()
                 .getForEntity("http://localhost:1111/naser", String.class);
