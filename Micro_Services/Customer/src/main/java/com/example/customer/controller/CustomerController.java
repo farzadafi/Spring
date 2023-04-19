47370564c0d89dfe1325daedf08edc737fc2be94
farzadafi <farzadafi40@gmail.com>
2023-04-19 10:00:05 +0330

    add registerCustomer method

--- Micro_Services/Customer/src/main/java/com/example/customer/controller/CustomerController.java
+++ Micro_Services/Customer/src/main/java/com/example/customer/controller/CustomerController.java
@@ -1,5 +1,21 @@
 package com.example.customer.controller;
 
-public record CustomerController() {
+import com.example.customer.dto.CustomerRegistrationRequest;
+import com.example.customer.service.CustomerService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/customers")
+public record CustomerController(CustomerService customerService) {
+
+    @PostMapping
+    public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRequest) {
+        log.info("new Customer registration {}", customerRequest);
+        customerService.registerCustomer(customerRequest);
+    }
 }
