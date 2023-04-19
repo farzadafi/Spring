bfe59d905828b83ea29e53ea43fdc70bebc60db8
farzadafi <farzadafi40@gmail.com>
2023-04-19 09:59:46 +0330

    add registerCustomer method

--- Micro_Services/Customer/src/main/java/com/example/customer/service/CustomerService.java
+++ Micro_Services/Customer/src/main/java/com/example/customer/service/CustomerService.java
@@ -1,4 +1,17 @@
 package com.example.customer.service;
 
+import com.example.customer.dto.CustomerRegistrationRequest;
+import com.example.customer.model.Customer;
+import org.springframework.stereotype.Service;
+
+@Service
 public record CustomerService() {
+
+    public void registerCustomer(CustomerRegistrationRequest customerRequest) {
+        Customer customer = Customer.builder()
+                .firstname(customerRequest.firstname())
+                .lastname(customerRequest.lastname())
+                .email(customerRequest.email())
+                .build();
+    }
 }
