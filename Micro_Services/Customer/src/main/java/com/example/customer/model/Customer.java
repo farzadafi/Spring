b7119f9ae05c7af6b30619e50915c54ec115a7b2
farzadafi <farzadafi40@gmail.com>
2023-04-22 20:54:26 +0330

    add Entity annotation

--- Micro_Services/Customer/src/main/java/com/example/customer/model/Customer.java
+++ Micro_Services/Customer/src/main/java/com/example/customer/model/Customer.java
@@ -1,5 +1,6 @@
 package com.example.customer.model;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 @Getter
@@ -7,7 +8,17 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
 public class Customer {
+    @Id
+    @SequenceGenerator(
+            name = "customer_id_sequence",
+            sequenceName = "customer_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "customer_id_sequence"
+    )
     private Integer id;
     private String firstname;
     private String lastname;
