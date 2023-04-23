d83ba0108d4b240748d5b3cbae47229cb12a0c81
farzadafi <farzadafi40@gmail.com>
2023-04-23 15:33:48 +0330

    fix registerMethod and call save of customerRepository

--- Micro_Services/Customer/src/main/java/com/example/customer/service/CustomerService.java
+++ Micro_Services/Customer/src/main/java/com/example/customer/service/CustomerService.java
@@ -2,10 +2,11 @@ package com.example.customer.service;
 
 import com.example.customer.dto.CustomerRegistrationRequest;
 import com.example.customer.model.Customer;
+import com.example.customer.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
 
     public void registerCustomer(CustomerRegistrationRequest customerRequest) {
         Customer customer = Customer.builder()
@@ -13,5 +14,6 @@ public record CustomerService() {
                 .lastname(customerRequest.lastname())
                 .email(customerRequest.email())
                 .build();
+        customerRepository.save(customer);
     }
 }
