d68302047ade7ed0a27d6eded15d23c0333d3e21
farzadafi <farzadafi40@gmail.com>
2023-04-23 19:37:39 +0330

    fix JpaRepository

--- Micro_Services/Fraud/src/main/java/com/example/fraud/repository/FraudCheckHistoryRepository.java
+++ Micro_Services/Fraud/src/main/java/com/example/fraud/repository/FraudCheckHistoryRepository.java
@@ -1,6 +1,7 @@
 package com.example.fraud.repository;
 
+import com.example.fraud.model.FraudCheckHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface FraudCheckHistoryRepository extends JpaRepository<FraudCheckHistoryRepository, Integer> {
+public interface FraudCheckHistoryRepository extends JpaRepository<FraudCheckHistory, Integer> {
 }
