15f7e6a633b0cbd1e9b6f39ca27b497e726473a1
farzadafi <farzadafi40@gmail.com>
2023-04-23 19:32:23 +0330

    create FraudCheckHistoryRepository interface and extends form JpaRepository

---
+++ Micro_Services/Fraud/src/main/java/com/example/fraud/repository/FraudCheckHistoryRepository.java
@@ -0,0 +1,6 @@
+package com.example.fraud.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FraudCheckHistoryRepository extends JpaRepository<FraudCheckHistoryRepository, Integer> {
+}
