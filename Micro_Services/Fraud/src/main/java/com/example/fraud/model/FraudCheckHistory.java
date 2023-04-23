3f224e0e9da1181d5d1dab1d69bc8efa1da8f06d
farzadafi <farzadafi40@gmail.com>
2023-04-23 17:42:47 +0330

    create and add some field

---
+++ Micro_Services/Fraud/src/main/java/com/example/fraud/model/FraudCheckHistory.java
@@ -0,0 +1,30 @@
+package com.example.fraud.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class FraudCheckHistory {
+    @Id
+    @SequenceGenerator(
+            name = "fraud_id_sequence",
+            sequenceName = "fraud_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "fraud_id_sequence"
+    )
+    private Integer id;
+    private Integer customerId;
+    private Boolean isFraudster;
+    private LocalDateTime createdAt;
+}
