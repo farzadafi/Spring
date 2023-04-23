6f14f9a8cca249438688517bda0842cb336363bb
farzadafi <farzadafi40@gmail.com>
2023-04-23 17:09:38 +0330

    create Fraud module

---
+++ Micro_Services/Fraud/src/test/java/com/example/fraud/FraudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fraud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FraudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
