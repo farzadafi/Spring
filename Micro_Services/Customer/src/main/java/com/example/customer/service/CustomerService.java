fd19bc713c9e0b866aa46489049f6b6b28de0cfe
farzadafi <farzadafi40@gmail.com>
2023-04-23 20:38:14 +0330

    fix registerCustomer method and add new functionality for chekc fraud

--- Micro_Services/Customer/src/main/java/com/example/customer/service/CustomerService.java
+++ Micro_Services/Customer/src/main/java/com/example/customer/service/CustomerService.java
@@ -1,12 +1,15 @@
 package com.example.customer.service;
 
 import com.example.customer.dto.CustomerRegistrationRequest;
+import com.example.customer.dto.FraudCheckResponse;
 import com.example.customer.model.Customer;
 import com.example.customer.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+public record CustomerService(CustomerRepository customerRepository,
+                              RestTemplate restTemplate) {
 
     public void registerCustomer(CustomerRegistrationRequest customerRequest) {
         Customer customer = Customer.builder()
@@ -14,6 +17,15 @@ public record CustomerService(CustomerRepository customerRepository) {
                 .lastname(customerRequest.lastname())
                 .email(customerRequest.email())
                 .build();
-        customerRepository.save(customer);
+        customerRepository.saveAndFlush(customer);
+
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+
+        if(fraudCheckResponse.isFraudster())
+            throw new IllegalStateException("fraudster");
     }
 }
