b0b1e8c238445ef3c6876f7dc9f6ef4391a32b39
farzadafi <farzadafi40@gmail.com>
2023-04-25 15:04:52 +0330

    fix checkFraud method

--- Micro_Services/Customer/src/main/java/com/example/customer/service/CustomerService.java
+++ Micro_Services/Customer/src/main/java/com/example/customer/service/CustomerService.java
@@ -1,15 +1,17 @@
 package com.example.customer.service;
 
 import com.example.customer.dto.CustomerRegistrationRequest;
-import com.example.customer.dto.FraudCheckResponse;
 import com.example.customer.model.Customer;
 import com.example.customer.repository.CustomerRepository;
+import com.farzadafi.clients.fraud.FraudCheckResponse;
+import com.farzadafi.clients.fraud.FraudClient;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 @Service
 public record CustomerService(CustomerRepository customerRepository,
-                              RestTemplate restTemplate) {
+                              RestTemplate restTemplate,
+                              FraudClient fraudClient) {
 
     public void registerCustomer(CustomerRegistrationRequest customerRequest) {
         Customer customer = Customer.builder()
@@ -23,13 +25,9 @@ public record CustomerService(CustomerRepository customerRepository,
     }
 
     private void checkFraud(Customer customer) {
-        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
-                "http://FRAUD/api/v1/fraud-check/{customerId}",
-                FraudCheckResponse.class,
-                customer.getId()
-        );
+        FraudCheckResponse fraudster = fraudClient.isFraudster(customer.getId());
 
-        if (fraudCheckResponse.isFraudster())
+        if (fraudster.isFraudster())
             throw new IllegalStateException("fraudster");
     }
 }
