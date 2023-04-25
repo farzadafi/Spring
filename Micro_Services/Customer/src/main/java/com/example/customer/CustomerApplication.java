284b5ff6f5bf48c0f3f4b52ae5d577a4f2d1e076
farzadafi <farzadafi40@gmail.com>
2023-04-25 15:01:31 +0330

    add @EnableFeignClients

--- Micro_Services/Customer/src/main/java/com/example/customer/CustomerApplication.java
+++ Micro_Services/Customer/src/main/java/com/example/customer/CustomerApplication.java
@@ -3,9 +3,13 @@ package com.example.customer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients(
+        basePackages = "com.farzadafi.clients"
+)
 public class CustomerApplication {
     public static void main(String[] args) {
         SpringApplication.run(CustomerApplication.class, args);
