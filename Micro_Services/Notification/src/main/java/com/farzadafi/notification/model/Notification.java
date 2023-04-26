46ad804075f42f06cabcf83293a7ffce4c1fb716
farzadafi <farzadafi40@gmail.com>
2023-04-26 15:49:50 +0330

    create Notification model

---
+++ Micro_Services/Notification/src/main/java/com/farzadafi/notification/model/Notification.java
@@ -0,0 +1,32 @@
+package com.farzadafi.notification.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "notification")
+public class Notification {
+
+    @Id
+    @SequenceGenerator(
+            name = "notification_id_sequence",
+            sequenceName = "notification_id_sequence"
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "notification_id_sequence"
+    )
+    private Integer notificationId;
+    private Integer toCustomerId;
+    private String toCustomerEmail;
+    private String sender;
+    private String message;
+    private LocalDateTime sendAt;
+}
