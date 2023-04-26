a7062378bd3fffcef9dc6fe724c426baba7cb42b
farzadafi <farzadafi40@gmail.com>
2023-04-26 16:30:44 +0330

    fix registerCustomer and add sendNotification method

--- Micro_Services/Customer/src/main/java/com/example/customer/service/CustomerService.java
+++ Micro_Services/Customer/src/main/java/com/example/customer/service/CustomerService.java
@@ -5,13 +5,18 @@ import com.example.customer.model.Customer;
 import com.example.customer.repository.CustomerRepository;
 import com.farzadafi.clients.fraud.FraudCheckResponse;
 import com.farzadafi.clients.fraud.FraudClient;
+import com.farzadafi.clients.notification.NotificationClient;
+import com.farzadafi.clients.notification.NotificationRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.LocalDateTime;
 
 @Service
 public record CustomerService(CustomerRepository customerRepository,
                               RestTemplate restTemplate,
-                              FraudClient fraudClient) {
+                              FraudClient fraudClient,
+                              NotificationClient notificationClient) {
 
     public void registerCustomer(CustomerRegistrationRequest customerRequest) {
         Customer customer = Customer.builder()
@@ -22,6 +27,16 @@ public record CustomerService(CustomerRepository customerRepository,
         customerRepository.saveAndFlush(customer);
 
         checkFraud(customer);
+        sendNotification(customer);
+    }
+
+    private void sendNotification(Customer customer) {
+        NotificationRequest notificationRequest = new NotificationRequest(null, customer.getId(),
+                customer.getEmail(),
+                "farzadafi",
+                String.format("Hi %s, welcome to farzadafi site...", customer.getFirstname()),
+                LocalDateTime.now());
+        notificationClient.sendNotification(notificationRequest);
     }
 
     private void checkFraud(Customer customer) {
