3bfe99d5eaf6dc091b72366f5a219a6ea7d951f4
farzadafi <farzadafi40@gmail.com>
2023-04-26 15:58:25 +0330

    create NotificationController and add sendNotification method

---
+++ Micro_Services/Notification/src/main/java/com/farzadafi/notification/controller/NotificationController.java
@@ -0,0 +1,19 @@
+package com.farzadafi.notification.controller;
+
+import com.farzadafi.clients.notification.NotificationRequest;
+import com.farzadafi.notification.service.NotificationService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public record NotificationController(NotificationService notificationService) {
+
+    @PostMapping
+    public void sendNotification(@RequestBody NotificationRequest notificationRequest) {
+        log.info("New notification {}", notificationRequest);
+        notificationService.send(notificationRequest);
+    }
+}
