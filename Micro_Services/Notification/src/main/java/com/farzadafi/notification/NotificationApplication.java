43ed4b0faad0a8e9952a57dca2d5d720abbfeeac
farzadafi <farzadafi40@gmail.com>
2023-04-26 16:14:07 +0330

    add @EnableDiscoveryClient and @EnableFeignClients annotation

--- Micro_Services/Notification/src/main/java/com/farzadafi/notification/NotificationApplication.java
+++ Micro_Services/Notification/src/main/java/com/farzadafi/notification/NotificationApplication.java
@@ -2,8 +2,14 @@ package com.farzadafi.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients(
+        basePackages = "com.farzadafi.clients"
+)
 public class NotificationApplication {
 
     public static void main(String[] args) {
