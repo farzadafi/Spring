603ea52e301f60e15bf399af42e9e4aeb2e9958b
farzadafi <farzadafi40@gmail.com>
2023-05-01 16:07:32 +0330

    add @EnableDiscoveryClient annotation

--- Micro_Services/ApiGateway/src/main/java/com/farzadafi/apigateway/ApiGatewayApplication.java
+++ Micro_Services/ApiGateway/src/main/java/com/farzadafi/apigateway/ApiGatewayApplication.java
@@ -2,8 +2,10 @@ package com.farzadafi.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
