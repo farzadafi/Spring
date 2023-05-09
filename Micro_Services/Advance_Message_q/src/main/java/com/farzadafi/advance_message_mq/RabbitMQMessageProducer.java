435952074b7666bdf6a56ed1e91e73b96d8f4f52
farzadafi <farzadafi40@gmail.com>
2023-05-09 16:57:42 +0330

    create RabbitMQMessageProducer and add publish method

---
+++ Micro_Services/Advance_Message_q/src/main/java/com/farzadafi/advance_message_mq/RabbitMQMessageProducer.java
@@ -0,0 +1,22 @@
+package com.farzadafi.advance_message_mq;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class RabbitMQMessageProducer {
+
+    private final AmqpTemplate amqpTemplate;
+
+    public RabbitMQMessageProducer(AmqpTemplate amqpTemplate) {
+        this.amqpTemplate = amqpTemplate;
+    }
+
+    public void publish(Object payload, String exchange, String routingKey) {
+        log.info("Publishing to {} using routingKey {}. Payload: {}", exchange, routingKey, payload);
+        amqpTemplate.convertAndSend(exchange, routingKey, payload);
+        log.info("Published to {} using routingKey {}. Payload: {}", exchange, routingKey, payload);
+    }
+}
