032800ce376e877deeb71d04309f02ab92623469
farzadafi <farzadafi40@gmail.com>
2023-05-09 16:32:17 +0330

    create NotificationConfig and add some field for config rabbitmq

---
+++ Micro_Services/Notification/src/main/java/com/farzadafi/notification/config/NotificationConfig.java
@@ -0,0 +1,19 @@
+package com.farzadafi.notification.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Getter
+public class NotificationConfig {
+
+    @Value("${rabbitmq.exchanges.internal}")
+    private String internalExchange;
+
+    @Value("${rabbitmq.queues.notification}")
+    private String notificationQueue;
+
+    @Value("${rabbitmq.routing-keys.internal-notification}")
+    private String internalNotificationRoutingKey;
+}
