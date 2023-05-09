92bac4b74823961f3033915136036579176d5b5a
farzadafi <farzadafi40@gmail.com>
2023-05-09 16:07:54 +0330

    add connectionFactory method

--- Micro_Services/Advance_Message_q/src/main/java/com/farzadafi/advance_message_mq/RabbitMQConfig.java
+++ Micro_Services/Advance_Message_q/src/main/java/com/farzadafi/advance_message_mq/RabbitMQConfig.java
@@ -1,10 +1,12 @@
 package com.farzadafi.advance_message_mq;
 
 import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +17,28 @@ public class RabbitMQConfig {
 
     public RabbitMQConfig(ConnectionFactory connectionFactory) {
         this.connectionFactory = connectionFactory;
+    }
+
+    @Value("${spring.rabbitmq.host}")
+    private String rabbitHost;
+
+    @Value("${spring.rabbitmq.port}")
+    private int rabbitPort;
+
+    @Value("${spring.rabbitmq.username}")
+    private String rabbitUsername;
+
+    @Value("${spring.rabbitmq.password}")
+    private String rabbitPassword;
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        final CachingConnectionFactory factory = new CachingConnectionFactory();
+        factory.setHost(rabbitHost);
+        factory.setPort(rabbitPort);
+        factory.setUsername(rabbitUsername);
+        factory.setPassword(rabbitPassword);
+        return factory;
     }
 
     @Bean
