f024673b6813ff97c7605d0a3ffd9001da9468f5
farzadafi <farzadafi40@gmail.com>
2023-05-09 16:53:38 +0330

    add some method for config rabbitmq

--- Micro_Services/Notification/src/main/java/com/farzadafi/notification/config/NotificationConfig.java
+++ Micro_Services/Notification/src/main/java/com/farzadafi/notification/config/NotificationConfig.java
@@ -1,7 +1,12 @@
 package com.farzadafi.notification.config;
 
 import lombok.Getter;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -16,4 +21,22 @@ public class NotificationConfig {
 
     @Value("${rabbitmq.routing-keys.internal-notification}")
     private String internalNotificationRoutingKey;
+
+    @Bean
+    public TopicExchange internalTopicExchange() {
+        return new TopicExchange(this.internalExchange);
+    }
+
+    @Bean
+    public Queue notificatoinQueue() {
+        return new Queue(this.notificationQueue);
+    }
+
+    @Bean
+    public Binding internalToNotificationBinding() {
+        return BindingBuilder
+                .bind(notificatoinQueue())
+                .to(internalTopicExchange())
+                .with(this.internalNotificationRoutingKey);
+    }
 }
