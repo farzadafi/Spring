67e6c2b44c392032e50f894b64a4d82833f6ed20
farzadafi <farzadafi40@gmail.com>
2023-05-10 15:48:26 +0330

    fix connectionFactory field

--- Micro_Services/Advance_Message_q/src/main/java/com/farzadafi/advance_message_mq/RabbitMQConfig.java
+++ Micro_Services/Advance_Message_q/src/main/java/com/farzadafi/advance_message_mq/RabbitMQConfig.java
@@ -3,43 +3,19 @@ package com.farzadafi.advance_message_mq;
 import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
 
-    private final ConnectionFactory connectionFactory;
+    private final CachingConnectionFactory connectionFactory;
 
-    public RabbitMQConfig(ConnectionFactory connectionFactory) {
+    public RabbitMQConfig(CachingConnectionFactory connectionFactory) {
         this.connectionFactory = connectionFactory;
-    }
-
-    @Value("${spring.rabbitmq.host}")
-    private String rabbitHost;
-
-    @Value("${spring.rabbitmq.port}")
-    private int rabbitPort;
-
-    @Value("${spring.rabbitmq.username}")
-    private String rabbitUsername;
-
-    @Value("${spring.rabbitmq.password}")
-    private String rabbitPassword;
-
-    @Bean
-    public ConnectionFactory connectionFactory() {
-        final CachingConnectionFactory factory = new CachingConnectionFactory();
-        factory.setHost(rabbitHost);
-        factory.setPort(rabbitPort);
-        factory.setUsername(rabbitUsername);
-        factory.setPassword(rabbitPassword);
-        return factory;
     }
 
     @Bean
