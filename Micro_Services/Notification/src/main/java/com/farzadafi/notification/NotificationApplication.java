6405e2fef1c1889dc76ba991c69eb82feb237997
farzadafi <farzadafi40@gmail.com>
2023-05-10 15:47:11 +0330

    add a new Bean for command line ruuner

--- Micro_Services/Notification/src/main/java/com/farzadafi/notification/NotificationApplication.java
+++ Micro_Services/Notification/src/main/java/com/farzadafi/notification/NotificationApplication.java
@@ -1,11 +1,18 @@
 package com.farzadafi.notification;
 
+import com.farzadafi.advance_message_mq.RabbitMQMessageProducer;
+import com.farzadafi.notification.config.NotificationConfig;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "com.farzadafi.notification",
+        "com.farzadafi.advance_message_mq"
+})
 @EnableDiscoveryClient
 @EnableFeignClients(
         basePackages = "com.farzadafi.clients"
@@ -14,5 +21,12 @@ public class NotificationApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer, NotificationConfig notificationConfig) {
+        return args -> producer.publish("farzad",
+                notificationConfig.getInternalExchange(),
+                notificationConfig.getInternalNotificationRoutingKey());
     }
 }
