d1569044d02cfb705b581faa910d61429e11fb7d
farzadafi <farzadafi40@gmail.com>
2023-05-16 17:07:25 +0330

    create NotificationConsumer and add consumer method

---
+++ Micro_Services/Notification/src/main/java/com/farzadafi/notification/rabbitmq/NotificationConsumer.java
@@ -0,0 +1,18 @@
+package com.farzadafi.notification.rabbitmq;
+
+import com.farzadafi.clients.notification.NotificationRequest;
+import com.farzadafi.notification.service.NotificationService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public record NotificationConsumer(NotificationService notificationService) {
+
+    @RabbitListener(queues = "${rabbitmq.queues.notification}")
+    public void consumer(NotificationRequest notificationRequest) {
+        log.info("Consumed {} from queue", notificationRequest);
+        notificationService.send(notificationRequest);
+    }
+}
