bf74ef2bd8dd14e39f32f3a9d85819dba3c08b21
farzadafi <farzadafi40@gmail.com>
2023-05-17 10:38:08 +0330

    comment commandline runner code

--- Micro_Services/Notification/src/main/java/com/farzadafi/notification/NotificationApplication.java
+++ Micro_Services/Notification/src/main/java/com/farzadafi/notification/NotificationApplication.java
@@ -1,13 +1,9 @@
 package com.farzadafi.notification;
 
-import com.farzadafi.advance_message_mq.RabbitMQMessageProducer;
-import com.farzadafi.notification.config.NotificationConfig;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication(scanBasePackages = {
         "com.farzadafi.notification",
@@ -23,10 +19,10 @@ public class NotificationApplication {
         SpringApplication.run(NotificationApplication.class, args);
     }
 
-    @Bean
-    CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer, NotificationConfig notificationConfig) {
-        return args -> producer.publish("farzad",
-                notificationConfig.getInternalExchange(),
-                notificationConfig.getInternalNotificationRoutingKey());
-    }
+//    @Bean
+//    CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer, NotificationConfig notificationConfig) {
+//        return args -> producer.publish("farzad",
+//                notificationConfig.getInternalExchange(),
+//                notificationConfig.getInternalNotificationRoutingKey());
+//    }
 }
