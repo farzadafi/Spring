f09abea77e20cb970b2f00a9737069abe97b9809
farzadafi <farzadafi40@gmail.com>
2023-05-24 04:47:38 +0330

    fix rabbitmq connection field

--- Micro_Services/Advance_Message_q/src/main/java/com/farzadafi/advance_message_mq/ConnectFactory.java
+++ Micro_Services/Advance_Message_q/src/main/java/com/farzadafi/advance_message_mq/ConnectFactory.java
@@ -1,32 +1,32 @@
 package com.farzadafi.advance_message_mq;
 
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ConnectFactory {
 
-    @Value("${spring.rabbitmq.host}")
-    private String rabbitHost;
+    //    @Value("${spring.rabbitmq.host}")
+    private final static String rabbitHost = "rabbitmq";
+    //when use docker on rabbitmq, when use locally should be localhost
 
-    @Value("${spring.rabbitmq.port}")
-    private int rabbitPort;
+    //    @Value("${spring.rabbitmq.port}")
+    private final static int RABBIT_PORT = 5672;
 
-    @Value("${spring.rabbitmq.username}")
-    private String rabbitUsername;
+    //    @Value("${spring.rabbitmq.username}")
+    private final static String RABBIT_USERNAME = "guest";
 
-    @Value("${spring.rabbitmq.password}")
-    private String rabbitPassword;
+    //    @Value("${spring.rabbitmq.password}")
+    private final static String RABBIT_PASSWORD = "guest";
 
-        @Bean
+    @Bean
     public CachingConnectionFactory connectionFactory() {
         final CachingConnectionFactory factory = new CachingConnectionFactory();
         factory.setHost(rabbitHost);
-        factory.setPort(rabbitPort);
-        factory.setUsername(rabbitUsername);
-        factory.setPassword(rabbitPassword);
+        factory.setPort(RABBIT_PORT);
+        factory.setUsername(RABBIT_USERNAME);
+        factory.setPassword(RABBIT_PASSWORD);
         return factory;
     }
 }
