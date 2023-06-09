41a401fb7857cca98ee9ed40a09e2d8825d967c3
farzadafi <farzadafi40@gmail.com>
2023-06-09 18:24:26 +0330

    add @Configuration annotation

--- Spring_Cloud/2_MicroService_SpringCloud/api-gateway/src/main/java/com/example/apigateway/LoggingFilter.java
+++ Spring_Cloud/2_MicroService_SpringCloud/api-gateway/src/main/java/com/example/apigateway/LoggingFilter.java
@@ -4,9 +4,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+@Configuration
 public class LoggingFilter implements GlobalFilter {
 
     private final Logger logger = LoggerFactory.getLogger(LoggingFilter.class);
