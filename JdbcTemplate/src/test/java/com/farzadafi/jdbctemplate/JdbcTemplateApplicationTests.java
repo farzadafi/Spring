14cf5a7ad1a75625dfe5e5e512b2a7701f59a8ea
farzadafi <farzadafi40@gmail.com>
2023-06-22 20:57:24 +0330

    create JdbcTemplate module

---
+++ JdbcTemplate/src/test/java/com/farzadafi/jdbctemplate/JdbcTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farzadafi.jdbctemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
