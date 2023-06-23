7ed2b31ece37fc50bcf63c4b34eb76f63075491a
farzadafi <farzadafi40@gmail.com>
2023-06-23 19:58:31 +0330

    create UniversityBatchSetter and implement from BatchPreparedStatementSetter and override method

---
+++ JdbcTemplate/src/main/java/com/farzadafi/jdbctemplate/repository/batchSetter/UniversityBatchSetter.java
@@ -0,0 +1,30 @@
+package com.farzadafi.jdbctemplate.repository.batchSetter;
+
+import com.farzadafi.jdbctemplate.entity.University;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+public class UniversityBatchSetter implements BatchPreparedStatementSetter {
+
+    private final List<University> universities;
+
+    public UniversityBatchSetter(List<University> universities) {
+        this.universities = universities;
+    }
+
+    @Override
+    public void setValues(PreparedStatement ps, int i) throws SQLException {
+        University university = universities.get(i);
+        ps.setInt(1, university.getId());
+        ps.setString(2, university.getName());
+        ps.setString(3, university.getAddress());
+    }
+
+    @Override
+    public int getBatchSize() {
+        return universities.size();
+    }
+}
