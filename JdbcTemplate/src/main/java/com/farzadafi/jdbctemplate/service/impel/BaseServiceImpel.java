c332758d49fc75085f44bb49507a4e6519b2a279
farzadafi <farzadafi40@gmail.com>
2023-06-23 15:00:50 +0330

    implements body of all methods

--- JdbcTemplate/src/main/java/com/farzadafi/jdbctemplate/service/impel/BaseServiceImpel.java
+++ JdbcTemplate/src/main/java/com/farzadafi/jdbctemplate/service/impel/BaseServiceImpel.java
@@ -12,28 +12,34 @@ public class BaseServiceImpel<ID extends Serializable, TYPE extends BaseEntity<I
         R extends BaseRepository<ID, TYPE>>
         implements BaseService<ID, TYPE> {
 
+    protected final R repository;
+
+    public BaseServiceImpel(R repository) {
+        this.repository = repository;
+    }
+
     @Override
     public void save(TYPE entity) throws SQLException {
-
+        repository.save(entity);
     }
 
     @Override
     public void saveAll(List<TYPE> entities) throws SQLException {
-
+        repository.saveAll(entities);
     }
 
     @Override
     public List<TYPE> findAll() throws SQLException {
-        return null;
+        return repository.findAll();
     }
 
     @Override
     public TYPE findById(ID id) throws SQLException {
-        return null;
+        return repository.findById(id);
     }
 
     @Override
     public void update(TYPE entity) throws SQLException {
-
+        repository.update(entity);
     }
 }
