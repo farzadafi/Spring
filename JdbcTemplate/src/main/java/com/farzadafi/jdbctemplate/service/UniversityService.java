281fa0df8f3d567fb4802eab2119700438021627
farzadafi <farzadafi40@gmail.com>
2023-06-23 20:06:38 +0330

    remove @Service

--- JdbcTemplate/src/main/java/com/farzadafi/jdbctemplate/service/UniversityService.java
+++ JdbcTemplate/src/main/java/com/farzadafi/jdbctemplate/service/UniversityService.java
@@ -2,8 +2,6 @@ package com.farzadafi.jdbctemplate.service;
 
 import com.farzadafi.jdbctemplate.base.service.BaseService;
 import com.farzadafi.jdbctemplate.entity.University;
-import org.springframework.stereotype.Service;
 
-@Service
 public interface UniversityService extends BaseService<Integer, University> {
 }
