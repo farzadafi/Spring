01e6a36b50dbc435952d364d9a808d6cde51d39f
farzadafi <farzadafi40@gmail.com>
2023-06-23 19:48:01 +0330

    create UniversityRepositoryImpel and extend from BaseRepositoryImpel and override all method

---
+++ JdbcTemplate/src/main/java/com/farzadafi/jdbctemplate/repository/impel/UniversityRepositoryImpel.java
@@ -0,0 +1,48 @@
+package com.farzadafi.jdbctemplate.repository.impel;
+
+import com.farzadafi.jdbctemplate.base.repository.BaseRepositoryImpel;
+import com.farzadafi.jdbctemplate.entity.University;
+import com.farzadafi.jdbctemplate.repository.UniversityRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Repository
+public class UniversityRepositoryImpel extends BaseRepositoryImpel<Integer, University> implements UniversityRepository {
+    protected UniversityRepositoryImpel(JdbcTemplate jdbcTemplate) {
+        super(jdbcTemplate);
+    }
+
+    @Override
+    public String getTableName() {
+        return "university";
+    }
+
+    @Override
+    public String getColumnsName() {
+        return "(id, name, address)";
+    }
+
+    @Override
+    public String getUpdateQueryParams() {
+        return "";
+    }
+
+    @Override
+    public String getCountOfQuestionMarkForParams() {
+        return "(?, ?, ?)";
+    }
+
+    @Override
+    public University mapResultSetToEntity(ResultSet resultSet) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public void fillParamForStatement(PreparedStatement preparedStatement, University entity, boolean isCountOnly) {
+
+    }
+}
