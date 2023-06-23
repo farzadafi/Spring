a8df3fdfdafe81b72e2b8b8a9c3369b365b9d195
farzadafi <farzadafi40@gmail.com>
2023-06-23 13:46:46 +0330

    add findAll and update methods

--- JdbcTemplate/src/main/java/com/farzadafi/jdbctemplate/repository/BaseRepository.java
+++ JdbcTemplate/src/main/java/com/farzadafi/jdbctemplate/repository/BaseRepository.java
@@ -4,9 +4,14 @@ import com.farzadafi.jdbctemplate.entity.base.BaseEntity;
 
 import java.io.Serializable;
 import java.sql.SQLException;
+import java.util.List;
 
 public interface BaseRepository<ID extends Serializable, TYPE extends BaseEntity<ID>> {
     void save(TYPE entity) throws SQLException;
 
     TYPE findById(ID id) throws SQLException;
+
+    List<TYPE> findAll() throws SQLException;
+
+    void update(TYPE entity) throws SQLException;
 }
