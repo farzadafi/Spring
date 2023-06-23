6a751f46cb2886cf8e6b7a71d59bc553e3c89298
farzadafi <farzadafi40@gmail.com>
2023-06-23 20:30:08 +0330

    change access modifier from private to protected

--- JdbcTemplate/src/main/java/com/farzadafi/jdbctemplate/base/model/BaseEntity.java
+++ JdbcTemplate/src/main/java/com/farzadafi/jdbctemplate/base/model/BaseEntity.java
@@ -5,10 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.io.Serializable;
 
@@ -20,5 +17,5 @@ import java.io.Serializable;
 public class BaseEntity <ID extends Serializable>{
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    private ID id;
+    protected ID id;
 }
