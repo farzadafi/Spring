0fba2abd5543dea26a02dde7061fa02981764bab
farzadafi <farzadafi40@gmail.com>
2023-06-23 20:31:43 +0330

    change ID type from Integer to long

--- JdbcTemplate/src/main/java/com/farzadafi/jdbctemplate/service/UniversityService.java
+++ JdbcTemplate/src/main/java/com/farzadafi/jdbctemplate/service/UniversityService.java
@@ -3,5 +3,8 @@ package com.farzadafi.jdbctemplate.service;
 import com.farzadafi.jdbctemplate.base.service.BaseService;
 import com.farzadafi.jdbctemplate.entity.University;
 
-public interface UniversityService extends BaseService<Integer, University> {
+import java.sql.SQLException;
+
+public interface UniversityService extends BaseService<Long, University> {
+    void insertData() throws SQLException;
 }
--- JdbcTemplate/src/main/java/com/farzadafi/jdbctemplate/service/impel/UniversityServiceImpel.java
+++ JdbcTemplate/src/main/java/com/farzadafi/jdbctemplate/service/impel/UniversityServiceImpel.java
@@ -3,14 +3,31 @@ package com.farzadafi.jdbctemplate.service.impel;
 import com.farzadafi.jdbctemplate.base.service.BaseServiceImpel;
 import com.farzadafi.jdbctemplate.entity.University;
 import com.farzadafi.jdbctemplate.repository.UniversityRepository;
+import com.farzadafi.jdbctemplate.repository.batchSetter.UniversityBatchSetter;
 import com.farzadafi.jdbctemplate.service.UniversityService;
 import org.springframework.stereotype.Service;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
-public class UniversityServiceImpel extends BaseServiceImpel<Integer, University, UniversityRepository>
+public class UniversityServiceImpel extends BaseServiceImpel<Long, University, UniversityRepository>
         implements UniversityService {
 
     public UniversityServiceImpel(UniversityRepository repository) {
         super(repository);
+    }
+
+    public void insertData() throws SQLException {
+        List<University> universities = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            University university = University.builder()
+                    .name("university" + i)
+                    .address("location " + i).build();
+            university.setId((long) i);
+            universities.add(university);
+        }
+        saveAll(universities, new UniversityBatchSetter(universities));
     }
 }
