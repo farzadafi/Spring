cc813340e64f8ce190431934bb6279bc25494c8a
farzadafi <farzadafi40@gmail.com>
2023-06-23 19:45:50 +0330

    move under base package and fix saveAll method signature

--- JdbcTemplate/src/main/java/com/farzadafi/jdbctemplate/base/service/BaseService.java
+++ JdbcTemplate/src/main/java/com/farzadafi/jdbctemplate/base/service/BaseService.java
@@ -1,13 +1,17 @@
-package com.farzadafi.jdbctemplate.service;
+package com.farzadafi.jdbctemplate.base.service;
+
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.stereotype.Service;
 
 import java.sql.SQLException;
 import java.util.List;
 
+@Service
 public interface BaseService <ID,TYPE>{
 
     void save(TYPE entity) throws SQLException;
 
-    void saveAll(List<TYPE> entities) throws SQLException;
+    void saveAll(List<TYPE> entities, BatchPreparedStatementSetter setter) throws SQLException;
 
     List<TYPE> findAll() throws SQLException;
 
