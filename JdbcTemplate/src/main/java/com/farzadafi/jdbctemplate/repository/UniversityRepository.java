ab4ce7acdb1cec1c87ee6403f589574533327aa8
farzadafi <farzadafi40@gmail.com>
2023-06-23 20:05:58 +0330

    remove @Service

--- JdbcTemplate/src/main/java/com/farzadafi/jdbctemplate/repository/UniversityRepository.java
+++ JdbcTemplate/src/main/java/com/farzadafi/jdbctemplate/repository/UniversityRepository.java
@@ -2,8 +2,6 @@ package com.farzadafi.jdbctemplate.repository;
 
 import com.farzadafi.jdbctemplate.base.repository.BaseRepository;
 import com.farzadafi.jdbctemplate.entity.University;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UniversityRepository extends BaseRepository<Integer, University> {
 }
