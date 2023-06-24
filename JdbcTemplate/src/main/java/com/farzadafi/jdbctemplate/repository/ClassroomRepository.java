93aaf80cb9e61bf903e13e4baf857e7ce701cda9
farzadafi <farzadafi40@gmail.com>
2023-06-24 04:40:27 +0330

    create ClassroomRepository and extend from BaseRepository

---
+++ JdbcTemplate/src/main/java/com/farzadafi/jdbctemplate/repository/ClassroomRepository.java
@@ -0,0 +1,7 @@
+package com.farzadafi.jdbctemplate.repository;
+
+import com.farzadafi.jdbctemplate.base.repository.BaseRepository;
+import com.farzadafi.jdbctemplate.entity.Classroom;
+
+public interface ClassroomRepository extends BaseRepository<Long, Classroom> {
+}
