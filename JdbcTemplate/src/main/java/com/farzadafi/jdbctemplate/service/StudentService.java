b12f34b7b55ba81d3d7949b531c96efa15fa9c32
farzadafi <farzadafi40@gmail.com>
2023-06-24 05:20:38 +0330

    create StudentService and extend from BaseService

---
+++ JdbcTemplate/src/main/java/com/farzadafi/jdbctemplate/service/StudentService.java
@@ -0,0 +1,7 @@
+package com.farzadafi.jdbctemplate.service;
+
+import com.farzadafi.jdbctemplate.base.service.BaseService;
+import com.farzadafi.jdbctemplate.entity.Student;
+
+public interface StudentService extends BaseService<Long, Student> {
+}
