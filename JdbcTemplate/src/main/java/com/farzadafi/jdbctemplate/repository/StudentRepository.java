e72ebe0e8a6243fea0668ed7e1a5f65a307f5cf8
farzadafi <farzadafi40@gmail.com>
2023-06-24 05:18:01 +0330

    create StudentRepository interface and extend from BaseRepository

---
+++ JdbcTemplate/src/main/java/com/farzadafi/jdbctemplate/repository/StudentRepository.java
@@ -0,0 +1,7 @@
+package com.farzadafi.jdbctemplate.repository;
+
+import com.farzadafi.jdbctemplate.base.repository.BaseRepository;
+import com.farzadafi.jdbctemplate.entity.Student;
+
+public interface StudentRepository extends BaseRepository<Long, Student> {
+}
