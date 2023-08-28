9ab63cfa8d82f9e2733271f36f82599ffc20b693
farzadafi <farzadafi40@gmail.com>
2023-08-28 16:45:40 +0330

    create resources-server module

---
+++ oauth/resources-server/src/test/java/com/farzadafi/resourcesserver/ResourcesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farzadafi.resourcesserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourcesServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
