1cadca8bc2179a434d0f0b67ae25abdc389c6dab
farzadafi <farzadafi40@gmail.com>
2023-08-28 16:02:11 +0330

    create oauth project

---
+++ oauth/src/main/java/com/farzadafi/oauth/OauthApplication.java
@@ -0,0 +1,13 @@
+package com.farzadafi.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthApplication.class, args);
+    }
+
+}
