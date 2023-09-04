d1af2020d35a32d1be6efa080c679aab624a64ff
farzadafi <farzadafi40@gmail.com>
2023-09-04 20:01:52 +0330

    add DefaultSecurityConfig class

---
+++ oauth2/auth-server/src/main/java/com/farzadafi/authserver/config/DefaultSecurityConfig.java
@@ -0,0 +1,7 @@
+package com.farzadafi.authserver.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class DefaultSecurityConfig {
+}
