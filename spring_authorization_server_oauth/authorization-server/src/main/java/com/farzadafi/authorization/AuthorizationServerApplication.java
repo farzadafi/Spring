12e1a5a10d22e2c79b3c84e9d101a205993ff2e4
farzadafi <farzadafi40@gmail.com>
2023-10-06 19:08:32 +0330

    add annotation and main class for spring

--- spring_authorization_server_oauth/authorization-server/src/main/java/com/farzadafi/authorization/AuthorizationServerApplication.java
+++ spring_authorization_server_oauth/authorization-server/src/main/java/com/farzadafi/authorization/AuthorizationServerApplication.java
@@ -1,4 +1,11 @@
 package com.farzadafi.authorization;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class AuthorizationServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationServerApplication.class, args);
+    }
 }
