b50d8f621ad43ddebada726dc6b72a3aea33f2ab
farzadafi <farzadafi40@gmail.com>
2023-10-06 19:11:02 +0330

    add annotation and main method for spring

--- spring_authorization_server_oauth/client-server/src/main/java/com/farzadafi/client/ClientServerApplication.java
+++ spring_authorization_server_oauth/client-server/src/main/java/com/farzadafi/client/ClientServerApplication.java
@@ -1,4 +1,11 @@
 package com.farzadafi.client;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class ClientServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ClientServerApplication.class, args);
+    }
 }
