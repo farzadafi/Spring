c01963e5df675cc64914255bab7f22a23f41f82a
farzadafi <farzadafi40@gmail.com>
2023-10-18 21:49:31 +0330

    add main method

--- spring-base/src/main/java/com/farzadafi/springbase/SpringBaseApplication.java
+++ spring-base/src/main/java/com/farzadafi/springbase/SpringBaseApplication.java
@@ -1,4 +1,11 @@
 package com.farzadafi.springbase;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SpringBaseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBaseApplication.class, args);
+    }
 }
