67b056aa8577364bf8dcc27eb9adba344e33b2ae
farzadafi <farzadafi40@gmail.com>
2023-10-19 19:28:34 +0330

    add commandLineRuner

--- spring-base/src/main/java/com/farzadafi/springbase/SpringBaseApplication.java
+++ spring-base/src/main/java/com/farzadafi/springbase/SpringBaseApplication.java
@@ -1,11 +1,20 @@
 package com.farzadafi.springbase;
 
+import com.farzadafi.springbase.model.Student;
+import com.farzadafi.springbase.service.StudentService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBaseApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBaseApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner (StudentService studentService){
+        return args -> studentService.register(new Student(null, null, null));
     }
 }
