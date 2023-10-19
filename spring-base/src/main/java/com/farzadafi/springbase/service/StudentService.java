cf59b3885fc0833bd39b57cc05d3f9265aab3ac4
farzadafi <farzadafi40@gmail.com>
2023-10-19 19:28:59 +0330

    fix register method and pass a student to it

--- spring-base/src/main/java/com/farzadafi/springbase/service/StudentService.java
+++ spring-base/src/main/java/com/farzadafi/springbase/service/StudentService.java
@@ -3,6 +3,5 @@ package com.farzadafi.springbase.service;
 import com.farzadafi.springbase.model.Student;
 
 public interface StudentService {
-    void register();
-    Student findByStudentNumber(String studentNumber);
+    void register(Student student);
 }
