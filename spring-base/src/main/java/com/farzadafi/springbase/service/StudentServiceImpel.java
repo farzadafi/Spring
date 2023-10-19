b98a247ecf673af5b59e2119b0f65ba5eb0857b1
farzadafi <farzadafi40@gmail.com>
2023-10-19 18:19:19 +0330

    add register and findByStudentNumber methods

--- spring-base/src/main/java/com/farzadafi/springbase/service/StudentServiceImpel.java
+++ spring-base/src/main/java/com/farzadafi/springbase/service/StudentServiceImpel.java
@@ -1,4 +1,30 @@
 package com.farzadafi.springbase.service;
 
-public class StudentServiceImpel {
+import com.farzadafi.springbase.model.Student;
+import com.farzadafi.springbase.repository.JdbcStudentRepository;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentServiceImpel implements StudentService {
+
+    private final JdbcStudentRepository repository;
+
+    public StudentServiceImpel(JdbcStudentRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    @PostConstruct
+    public void register() {
+        System.out.println("farzad");
+        Student student = new Student(null, "farzad", "3033333333");
+        repository.save(student);
+    }
+
+    @Override
+    public Student findByStudentNumber(String studentNumber) {
+        return repository.findByStudentNumber(studentNumber)
+                .orElseThrow(() -> new RuntimeException("username not found"));
+    }
 }
