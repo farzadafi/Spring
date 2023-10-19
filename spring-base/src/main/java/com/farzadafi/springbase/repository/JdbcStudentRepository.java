afe2ce516ff917c4ca3bb48a4ef0d425b34e4b37
farzadafi <farzadafi40@gmail.com>
2023-10-19 15:42:21 +0330

    create JdbcStudentRepository class

---
+++ spring-base/src/main/java/com/farzadafi/springbase/repository/JdbcStudentRepository.java
@@ -0,0 +1,60 @@
+package com.farzadafi.springbase.repository;
+
+import com.farzadafi.springbase.model.Student;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.Optional;
+
+@SuppressWarnings("query")
+@RequiredArgsConstructor
+public class JdbcStudentRepository implements StudentRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Override
+    public boolean existsByStudentNumber(String studentNumber) {
+        String sql = """
+                select  count(*)
+                from student
+                where student_number = ?
+                """;
+        Integer count = jdbcTemplate.queryForObject(
+                sql,
+                Integer.class,
+                studentNumber
+        );
+        return count != null && count == 1;
+    }
+
+    @Override
+    public void save(Student student) {
+        String sql = """
+                insert into student(name, student_number)
+                values (?, ?)
+                """;
+        jdbcTemplate.update(
+                sql,
+                student.getFullName(),
+                student.getStudentNumber()
+        );
+    }
+
+    @Override
+    public Optional<Student> findByStudentNumber(String studentNumber) {
+        String sql = """
+                select name,student_number 
+                from student
+                where student_number = ?
+                """;
+        return jdbcTemplate.query(
+                        sql,
+                        (rs, rowNum) -> new Student(null,
+                                rs.getString("name"),
+                                rs.getString("student_number")
+                        ),
+                        studentNumber)
+                .stream()
+                .map(Student.class::cast)
+                .findFirst();
+    }
+}
