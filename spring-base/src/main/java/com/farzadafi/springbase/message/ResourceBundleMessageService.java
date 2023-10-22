befc6fb5585ee788a494a33477fa480e8f1a47a2
farzadafi <farzadafi40@gmail.com>
2023-10-22 20:24:44 +0330

    implements from MessageService and implement getMessage method

--- spring-base/src/main/java/com/farzadafi/springbase/message/ResourceBundleMessageService.java
+++ spring-base/src/main/java/com/farzadafi/springbase/message/ResourceBundleMessageService.java
@@ -1,4 +1,17 @@
 package com.farzadafi.springbase.message;
 
-public class ResourceBundleMessageService {
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+
+import java.util.Locale;
+
+@RequiredArgsConstructor
+public class ResourceBundleMessageService implements MessageService{
+
+    private final MessageSource messageSource;
+
+    @Override
+    public String getMessage(String code, String language) {
+        return messageSource.getMessage(code, null, Locale.forLanguageTag(language));
+    }
 }
