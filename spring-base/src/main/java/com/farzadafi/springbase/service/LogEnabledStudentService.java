50c6c25f5575520649df4b0e2fc2e57eb863e2ee
farzadafi <farzadafi40@gmail.com>
2023-10-22 20:53:10 +0330

    fix register method on log line

--- spring-base/src/main/java/com/farzadafi/springbase/service/LogEnabledStudentService.java
+++ spring-base/src/main/java/com/farzadafi/springbase/service/LogEnabledStudentService.java
@@ -1,17 +1,23 @@
 package com.farzadafi.springbase.service;
 
+import com.farzadafi.springbase.message.MessageService;
 import com.farzadafi.springbase.model.Student;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 
 @Slf4j
 @RequiredArgsConstructor
 public class LogEnabledStudentService implements StudentService {
     private final StudentService service;
+    private final MessageService messageService;
+
+    @Value("${application.configs.language}")
+    private String language;
 
     @Override
     public void register(Student student) {
-        log.info("registration request {}", student);
+        log.info(messageService.getMessage("register.successful", language)+ " {}", student);
         service.register(student);
     }
 }
