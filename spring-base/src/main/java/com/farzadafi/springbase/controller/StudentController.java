cd4dd6ff4f9af9da25b0d894c95a8bc4adb35c1e
farzadafi <farzadafi40@gmail.com>
2023-10-24 08:21:10 +0330

    add register method

---
+++ spring-base/src/main/java/com/farzadafi/springbase/controller/StudentController.java
@@ -0,0 +1,23 @@
+package com.farzadafi.springbase.controller;
+
+import com.farzadafi.springbase.model.common.CommonStudent;
+import com.farzadafi.springbase.service.StudentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class StudentController {
+
+    private final StudentService service;
+
+    @PostMapping()
+    public ResponseEntity<String> register() {
+        CommonStudent commonStudent = new CommonStudent(null, "farzad", "303");
+        service.register(commonStudent);
+        return new ResponseEntity<>("OK", HttpStatus.CREATED);
+    }
+}
