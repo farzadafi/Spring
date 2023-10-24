cb4a095c260f6232f4e0187eaf95e7f73491db9a
farzadafi <farzadafi40@gmail.com>
2023-10-24 07:52:35 +0330

    add findByStudentNumber method

--- spring-base/src/main/java/com/farzadafi/springbase/repository/StudentJpaRepository.java
+++ spring-base/src/main/java/com/farzadafi/springbase/repository/StudentJpaRepository.java
@@ -1,7 +1,11 @@
 package com.farzadafi.springbase.repository;
 
-import com.farzadafi.springbase.model.Student;
+import com.farzadafi.springbase.model.common.CommonStudent;
+import com.farzadafi.springbase.model.jpa.StudentEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StudentJpaRepository extends JpaRepository<Student, Integer> {
+import java.util.Optional;
+
+public interface StudentJpaRepository extends JpaRepository<StudentEntity, Integer> {
+    Optional<CommonStudent> findByStudentNumber(String studentNumber);
 }
