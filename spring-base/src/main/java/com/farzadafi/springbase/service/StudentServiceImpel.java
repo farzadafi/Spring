10b9f89e38dea10e474f5d3b529e5611e3bf82eb
farzadafi <farzadafi40@gmail.com>
2023-10-24 07:53:09 +0330

    change Student to CommonStudent

--- spring-base/src/main/java/com/farzadafi/springbase/service/StudentServiceImpel.java
+++ spring-base/src/main/java/com/farzadafi/springbase/service/StudentServiceImpel.java
@@ -1,19 +1,21 @@
 package com.farzadafi.springbase.service;
 
-import com.farzadafi.springbase.model.Student;
+import com.farzadafi.springbase.model.common.CommonStudent;
 import com.farzadafi.springbase.repository.JdbcStudentRepository;
+import com.farzadafi.springbase.repository.StudentRepository;
 
 public class StudentServiceImpel implements StudentService {
 
-    private final JdbcStudentRepository repository;
+//    private final JdbcStudentRepository repository;
+    private final StudentRepository repository;
 
     public StudentServiceImpel(JdbcStudentRepository repository) {
         this.repository = repository;
     }
 
     @Override
-    public void register(Student student1) {
-        Student student = new Student(null, "farzad", "3033333333");
+    public void register(CommonStudent student1) {
+        CommonStudent student = new CommonStudent(null, "farzad", "3033333333");
         repository.save(student);
     }
 }
