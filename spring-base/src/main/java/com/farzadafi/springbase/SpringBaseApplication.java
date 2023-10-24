e5827195dc5f979ef16fc5523db7eca167239262
farzadafi <farzadafi40@gmail.com>
2023-10-24 08:15:23 +0330

    remove command line Runner

--- spring-base/src/main/java/com/farzadafi/springbase/SpringBaseApplication.java
+++ spring-base/src/main/java/com/farzadafi/springbase/SpringBaseApplication.java
@@ -1,20 +1,11 @@
 package com.farzadafi.springbase;
 
-import com.farzadafi.springbase.model.common.CommonStudent;
-import com.farzadafi.springbase.service.StudentService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBaseApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBaseApplication.class, args);
-    }
-
-    @Bean
-    CommandLineRunner commandLineRunner (StudentService studentService){
-        return args -> studentService.register(new CommonStudent(null, null, null));
     }
 }
